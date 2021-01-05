b7631844592348019c308990c87f84e91402965b
Lawrence Nahum <lawrence@greenaddress.it>
2021-01-05 14:36:16 +0100

    move the pinserver out, use https://github.com/Blockstream/blind_pin_server

--- pinserver/generateserverkey.py
+++
@@ -1,4 +0,0 @@
-from .server import PINServerECDH
-
-if __name__ == '__main__':
-    PINServerECDH.generate_server_key_pair()
--- pinserver/wsgi.py
+++
@@ -1,6 +0,0 @@
-from .flaskserver import app as flaskapp
-import os
-
-# this directory is not autocreated and forces the user to create it/mount it
-assert os.path.exists('pins')
-app = flaskapp
