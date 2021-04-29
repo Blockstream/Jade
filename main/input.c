Blockstream/Jade

commit 51b6306d1427e6100db0f31a22ef6c3c6c7696c0

add support for TTGO TDISPLAY

main/input.c
@@ -61,8 +61,57 @@ static inline void wheel_next()
     }
 }
 
-#if !defined CONFIG_BOARD_TYPE_M5_FIRE && !defined CONFIG_BOARD_TYPE_M5_BLACK_GRAY
+#if defined(CONFIG_BOARD_TYPE_M5_FIRE) || defined(CONFIG_BOARD_TYPE_M5_BLACK_GRAY)                                     \
+    || defined(CONFIG_BOARD_TYPE_TTGO_TDISPLAY)
+
+// M5 wheel_init() to mock jade wheel
+static bool button_A_pressed = false;
+static bool button_B_pressed = false;
+
+static void button_pressed(void* arg)
+{
+    bool* button = arg;
+    *button = true;
+}
+
+static void button_handle_two_buttonsA(void* arg)
+{
+    if (button_B_pressed) {
+        gui_front_click();
+    } else if (button_A_pressed) {
+        wheel_next();
+    }
+    button_B_pressed = false;
+    button_A_pressed = false;
+}
+
+static void button_handle_two_buttonsB(void* arg)
+{
+    if (button_A_pressed) {
+        gui_front_click();
+    } else if (button_B_pressed) {
+        wheel_prev();
+    }
+    button_B_pressed = false;
+    button_A_pressed = false;
+}
+
+void wheel_init()
+{
+    button_handle_t btn_handle_prev = iot_button_create(CONFIG_INPUT_BTN_A, BUTTON_ACTIVE_LOW);
+    iot_button_set_evt_cb(btn_handle_prev, BUTTON_CB_PUSH, button_pressed, &button_A_pressed);
+    iot_button_set_evt_cb(btn_handle_prev, BUTTON_CB_RELEASE, button_handle_two_buttonsA, NULL);
+
+    button_handle_t btn_handle_next = iot_button_create(CONFIG_INPUT_BTN_B, BUTTON_ACTIVE_LOW);
+    iot_button_set_evt_cb(btn_handle_next, BUTTON_CB_PUSH, button_pressed, &button_B_pressed);
+    iot_button_set_evt_cb(btn_handle_next, BUTTON_CB_RELEASE, button_handle_two_buttonsB, NULL);
+}
+
+#else
+
+// Jade proper wheel init
 static QueueHandle_t event_queue;
+
 void wheel_watch_task(void* info_void)
 {
     rotary_encoder_info_t* info = (rotary_encoder_info_t*)info_void;
@@ -88,25 +137,9 @@ void wheel_watch_task(void* info_void)
 
     vTaskDelete(NULL);
 }
-#else
-// Used in M5 only case in wheel_init()
-static void button_A(void* arg) { wheel_prev(); }
-
-static void button_B(void* arg) { wheel_next(); }
-#endif
 
 void wheel_init()
 {
-#if defined CONFIG_BOARD_TYPE_M5_FIRE || defined CONFIG_BOARD_TYPE_M5_BLACK_GRAY
-
-    button_handle_t btn_handle_prev = iot_button_create(CONFIG_INPUT_BTN_A, BUTTON_ACTIVE_LOW);
-    iot_button_set_evt_cb(btn_handle_prev, BUTTON_CB_RELEASE, button_A, NULL);
-    iot_button_add_custom_cb(btn_handle_prev, 1, button_A, NULL);
-
-    button_handle_t btn_handle_next = iot_button_create(CONFIG_INPUT_BTN_B, BUTTON_ACTIVE_LOW);
-    iot_button_set_evt_cb(btn_handle_next, BUTTON_CB_RELEASE, button_B, NULL);
-    iot_button_add_custom_cb(btn_handle_next, 1, button_B, NULL);
-#else
     // Initialise the rotary encoder device with the GPIOs for A and B signals
     rotary_encoder_info_t* info = (rotary_encoder_info_t*)JADE_MALLOC(sizeof(rotary_encoder_info_t));
     esp_err_t rc = rotary_encoder_init(info, ROT_ENC_A_GPIO, ROT_ENC_B_GPIO);
@@ -127,5 +160,6 @@ void wheel_init()
     const BaseType_t retval = xTaskCreatePinnedToCore(&wheel_watch_task, "wheel_watcher", 2 * 1024, info, 5, NULL, 1);
     JADE_ASSERT_MSG(
         retval == pdPASS, "Failed to create wheel_watcher task, xTaskCreatePinnedToCore() returned %d", retval);
-#endif
 }
+
+#endif

.gitlab-ci.yml
@@ -93,6 +93,13 @@ test_build_display_m5blackgray:
   after_script:
     - mv build build_display_m5blackgray
 
+test_build_display_ttgo_tdisplay:
+  extends: .test_build_template
+  before_script:
+    - cp configs/sdkconfig_display_ttgo_tdisplay.defaults sdkconfig.defaults
+  after_script:
+    - mv build build_display_ttgo_tdisplay
+
 test_build_jade_noblobs_ci:
   extends: .test_build_template
   before_script:

configs/sdkconfig_display_ttgo_tdisplay.defaults
@@ -1,0 +1,93 @@
+# CONFIG_APP_COMPILE_TIME_DATE is not set
+CONFIG_APP_EXCLUDE_PROJECT_NAME_VAR=y
+CONFIG_BOOTLOADER_LOG_LEVEL_NONE=y
+# CONFIG_BOOTLOADER_WDT_ENABLE is not set
+CONFIG_BOOTLOADER_APP_ROLLBACK_ENABLE=y
+CONFIG_BOOTLOADER_APP_ANTI_ROLLBACK=y
+CONFIG_ESPTOOLPY_FLASHSIZE_4MB=y
+CONFIG_PARTITION_TABLE_CUSTOM=y
+CONFIG_PARTITION_TABLE_OFFSET=0x9000
+CONFIG_DEBUG_MODE=y
+CONFIG_BOARD_TYPE_TTGO_TDISPLAY=y
+CONFIG_COMPILER_OPTIMIZATION_SIZE=y
+CONFIG_COMPILER_STACK_CHECK_MODE_STRONG=y
+CONFIG_COMPILER_WARN_WRITE_STRINGS=y
+CONFIG_BT_ENABLED=y
+CONFIG_BTDM_CTRL_BLE_MAX_CONN=1
+# CONFIG_BTDM_CTRL_FULL_SCAN_SUPPORTED is not set
+CONFIG_BT_NIMBLE_ENABLED=y
+# CONFIG_BT_NIMBLE_ROLE_CENTRAL is not set
+# CONFIG_BT_NIMBLE_ROLE_BROADCASTER is not set
+# CONFIG_BT_NIMBLE_ROLE_OBSERVER is not set
+# CONFIG_BT_NIMBLE_SM_LEGACY is not set
+CONFIG_BT_NIMBLE_SVC_GAP_DEVICE_NAME="j"
+CONFIG_BT_NIMBLE_GAP_DEVICE_NAME_MAX_LEN=11
+CONFIG_BT_NIMBLE_ATT_PREFERRED_MTU=517
+CONFIG_EFUSE_VIRTUAL=y
+CONFIG_EFUSE_CODE_SCHEME_COMPAT_NONE=y
+CONFIG_ESP32_REV_MIN_1=y
+CONFIG_ESP32_DEFAULT_CPU_FREQ_240=y
+CONFIG_ESP32_UNIVERSAL_MAC_ADDRESSES_TWO=y
+# CONFIG_ESP32_DEBUG_OCDAWARE is not set
+# CONFIG_ADC_CAL_EFUSE_TP_ENABLE is not set
+# CONFIG_ADC_CAL_EFUSE_VREF_ENABLE is not set
+# CONFIG_ADC_CAL_LUT_ENABLE is not set
+CONFIG_ESP_MAIN_TASK_STACK_SIZE=12288
+CONFIG_ESP_CONSOLE_UART_NONE=y
+# CONFIG_ESP_TASK_WDT is not set
+# CONFIG_ETH_USE_ESP32_EMAC is not set
+# CONFIG_ETH_USE_SPI_ETHERNET is not set
+# CONFIG_ESP_HTTP_CLIENT_ENABLE_HTTPS is not set
+# CONFIG_HTTPD_ERR_RESP_NO_DELAY is not set
+# CONFIG_ESP_NETIF_TCPIP_ADAPTER_COMPATIBLE_LAYER is not set
+CONFIG_ESP_SYSTEM_PANIC_SILENT_REBOOT=y
+# CONFIG_ESP32_WIFI_SW_COEXIST_ENABLE is not set
+CONFIG_ESP32_WIFI_STATIC_RX_BUFFER_NUM=2
+CONFIG_ESP32_WIFI_DYNAMIC_RX_BUFFER_NUM=0
+CONFIG_ESP32_WIFI_STATIC_TX_BUFFER=y
+CONFIG_ESP32_WIFI_STATIC_TX_BUFFER_NUM=6
+# CONFIG_ESP32_WIFI_AMPDU_TX_ENABLED is not set
+# CONFIG_ESP32_WIFI_AMPDU_RX_ENABLED is not set
+# CONFIG_ESP32_WIFI_NVS_ENABLED is not set
+CONFIG_ESP32_WIFI_MGMT_SBUF_NUM=6
+# CONFIG_ESP32_WIFI_IRAM_OPT is not set
+# CONFIG_ESP32_WIFI_RX_IRAM_OPT is not set
+# CONFIG_ESP32_PHY_CALIBRATION_AND_DATA_STORAGE is not set
+# CONFIG_FATFS_PER_FILE_CACHE is not set
+# CONFIG_FMB_COMM_MODE_ASCII_EN is not set
+# CONFIG_FREERTOS_INTERRUPT_BACKTRACE is not set
+CONFIG_FREERTOS_SUPPORT_STATIC_ALLOCATION=y
+CONFIG_FREERTOS_TIMER_TASK_STACK_DEPTH=2560
+CONFIG_LOG_DEFAULT_LEVEL_NONE=y
+# CONFIG_LOG_COLORS is not set
+# CONFIG_LWIP_NETIF_LOOPBACK is not set
+# CONFIG_MBEDTLS_ASYMMETRIC_CONTENT_LEN is not set
+# CONFIG_MBEDTLS_HARDWARE_MPI is not set
+# CONFIG_MBEDTLS_HAVE_TIME is not set
+# CONFIG_MBEDTLS_SSL_RENEGOTIATION is not set
+# CONFIG_MBEDTLS_SSL_PROTO_TLS1 is not set
+# CONFIG_MBEDTLS_SSL_PROTO_TLS1_1 is not set
+# CONFIG_MBEDTLS_SSL_PROTO_TLS1_2 is not set
+# CONFIG_MBEDTLS_SSL_ALPN is not set
+# CONFIG_MBEDTLS_CLIENT_SSL_SESSION_TICKETS is not set
+# CONFIG_MBEDTLS_SERVER_SSL_SESSION_TICKETS is not set
+# CONFIG_MBEDTLS_PEM_PARSE_C is not set
+# CONFIG_MBEDTLS_PEM_WRITE_C is not set
+# CONFIG_MBEDTLS_X509_CRL_PARSE_C is not set
+# CONFIG_MBEDTLS_X509_CSR_PARSE_C is not set
+# CONFIG_MQTT_PROTOCOL_311 is not set
+# CONFIG_MQTT_TRANSPORT_SSL is not set
+# CONFIG_MQTT_TRANSPORT_WEBSOCKET is not set
+CONFIG_NEWLIB_NANO_FORMAT=y
+CONFIG_OPENSSL_ASSERT_DO_NOTHING=y
+CONFIG_SPI_FLASH_VERIFY_WRITE=y
+CONFIG_SPIFFS_MAX_PARTITIONS=1
+# CONFIG_SPIFFS_CACHE is not set
+# CONFIG_SPIFFS_PAGE_CHECK is not set
+CONFIG_SPIFFS_GC_MAX_RUNS=1
+# CONFIG_SPIFFS_USE_MAGIC is not set
+# CONFIG_SPIFFS_USE_MTIME is not set
+# CONFIG_UNITY_ENABLE_FLOAT is not set
+# CONFIG_UNITY_ENABLE_DOUBLE is not set
+# CONFIG_UNITY_ENABLE_IDF_TEST_RUNNER is not set
+# CONFIG_VFS_SUPPORT_TERMIOS is not set

main/Kconfig.projbuild
@@ -32,6 +32,9 @@ config BOARD_TYPE_M5_FIRE
 config BOARD_TYPE_M5_BLACK_GRAY
   bool "M5Stack Black/Gray"
   depends on !ESP32_SPIRAM_SUPPORT
+config BOARD_TYPE_TTGO_TDISPLAY
+  bool "TTGO T-DISPLAY"
+  depends on !ESP32_SPIRAM_SUPPORT
 config BOARD_TYPE_DEV
   bool "Dev board"
 config BOARD_TYPE_CUSTOM
@@ -48,7 +51,7 @@ config DISP_INVERT_COLORS
 
 config HAS_AXP
   bool "Use the AXP192 power controller"
-  depends on !BOARD_TYPE_M5_FIRE && !BOARD_TYPE_M5_BLACK_GRAY
+  depends on !BOARD_TYPE_M5_FIRE && !BOARD_TYPE_M5_BLACK_GRAY && !BOARD_TYPE_TTGO_TDISPLAY
   help
       Jade (and some devices) come with the AXP192 to manage power to ESP, display, camera
   default n
@@ -70,16 +73,19 @@ menu "Input configuration"
     int "BTN A"
     range 0 40
     default 39 if BOARD_TYPE_M5_FIRE || BOARD_TYPE_M5_BLACK_GRAY
+    default 0 if BOARD_TYPE_TTGO_TDISPLAY
   config INPUT_BTN_B
     int "BTN B"
     range 0 40
     default 37 if BOARD_TYPE_M5_FIRE || BOARD_TYPE_M5_BLACK_GRAY
+    default 35 if BOARD_TYPE_TTGO_TDISPLAY
   config INPUT_WHEEL_SW
     int "Wheel SW"
     range 0 40
     default 33 if BOARD_TYPE_JADE
     default  0 if BOARD_TYPE_M5_FIRE || BOARD_TYPE_M5_BLACK_GRAY
     default 27 if BOARD_TYPE_DEV
+    default 2 if BOARD_TYPE_TTGO_TDISPLAY
   config INPUT_INVERT_WHEEL
     bool "Invert wheel"
     default n
@@ -90,6 +96,7 @@ menu "Input configuration"
     default 21 if BOARD_TYPE_JADE
     default 38 if BOARD_TYPE_M5_FIRE || BOARD_TYPE_M5_BLACK_GRAY
     default 35 if BOARD_TYPE_DEV
+    default 2 if BOARD_TYPE_TTGO_TDISPLAY
 endmenu
 
 menu "Camera configuration"
@@ -197,7 +204,7 @@ menu "TFT Display"
     int
     default 0 if DISP_DRIVER_ILI9341 || BOARD_TYPE_M5_FIRE || BOARD_TYPE_M5_BLACK_GRAY
     default 1 if DISP_DRIVER_ILI9488
-    default 2 if DISP_DRIVER_ST7789V || BOARD_TYPE_DEV || BOARD_TYPE_JADE
+    default 2 if DISP_DRIVER_ST7789V || BOARD_TYPE_DEV || BOARD_TYPE_JADE || BOARD_TYPE_TTGO_TDISPLAY
     default 3 if DISP_DRIVER_ST7735
     default 4 if DISP_DRIVER_ST7735R
     default 5 if DISP_DRIVER_ST7735B
@@ -227,19 +234,19 @@ menu "TFT Display"
   config DISP_INVERT_ROTATION1
     int "Invert rotation 1"
     range 0 1
-    default 1 if BOARD_TYPE_JADE
+    default 1 if BOARD_TYPE_JADE || BOARD_TYPE_TTGO_TDISPLAY
     default 0 if BOARD_TYPE_M5_FIRE || BOARD_TYPE_M5_BLACK_GRAY
     default 1 if BOARD_TYPE_DEV
   config DISP_INVERT_ROTATION2
     int "Invert rotation 2"
     range 0 1
-    default 0 if BOARD_TYPE_JADE
+    default 0 if BOARD_TYPE_JADE || BOARD_TYPE_TTGO_TDISPLAY
     default 1 if BOARD_TYPE_M5_FIRE || BOARD_TYPE_M5_BLACK_GRAY
     default 0 if BOARD_TYPE_DEV
   config DISP_ORIENTATION_DEFAULT
     int "Set orientation default"
     range 0 3
-    default 3 if BOARD_TYPE_JADE
+    default 3 if BOARD_TYPE_JADE || BOARD_TYPE_TTGO_TDISPLAY
     default 1 if BOARD_TYPE_M5_FIRE || BOARD_TYPE_M5_BLACK_GRAY
     default 0 if BOARD_TYPE_DEV
   config DISP_RGB_BGR
@@ -253,17 +260,19 @@ menu "TFT Display"
       default 0 if BOARD_TYPE_JADE
       default 19 if BOARD_TYPE_M5_FIRE || BOARD_TYPE_M5_BLACK_GRAY
       default 25 if BOARD_TYPE_DEV
+      default 2 if BOARD_TYPE_TTGO_TDISPLAY
     config DISP_PIN_NUM_MOSI
       int "PIN_NUM_MOSI"
       range 0 40
       default 15 if BOARD_TYPE_JADE
       default 23 if BOARD_TYPE_M5_FIRE || BOARD_TYPE_M5_BLACK_GRAY
       default 23 if BOARD_TYPE_DEV
+      default 19 if BOARD_TYPE_TTGO_TDISPLAY
     config DISP_PIN_NUM_CLK
       int "PIN_NUM_CLK"
       range 0 40
       default 14 if BOARD_TYPE_JADE
-      default 18 if BOARD_TYPE_M5_FIRE || BOARD_TYPE_M5_BLACK_GRAY
+      default 18 if BOARD_TYPE_M5_FIRE || BOARD_TYPE_M5_BLACK_GRAY || BOARD_TYPE_TTGO_TDISPLAY
       default 19 if BOARD_TYPE_DEV
     config DISP_PIN_NUM_CS
       int "PIN_NUM_CS"
@@ -271,12 +280,14 @@ menu "TFT Display"
       default 13 if BOARD_TYPE_JADE
       default 14 if BOARD_TYPE_M5_FIRE || BOARD_TYPE_M5_BLACK_GRAY
       default 22 if BOARD_TYPE_DEV
+      default 5 if BOARD_TYPE_TTGO_TDISPLAY
     config DISP_PIN_NUM_DC
       int "PIN_NUM_DC"
       range 0 40
       default 12 if BOARD_TYPE_JADE
       default 27 if BOARD_TYPE_M5_FIRE || BOARD_TYPE_M5_BLACK_GRAY
       default 21 if BOARD_TYPE_DEV
+      default 16 if BOARD_TYPE_TTGO_TDISPLAY
     config DISP_PIN_NUM_TCS
       int "PIN_NUM_TCS"
       range 0 40
@@ -287,12 +298,14 @@ menu "TFT Display"
       default 21 if BOARD_TYPE_JADE
       default 33 if BOARD_TYPE_M5_FIRE || BOARD_TYPE_M5_BLACK_GRAY
       default 18 if BOARD_TYPE_DEV
+      default 23 if BOARD_TYPE_TTGO_TDISPLAY
     config DISP_PIN_NUM_BCKL
       int "PIN_NUM_BCKL"
       range 0 40
       default 0 if BOARD_TYPE_JADE
       default 32 if BOARD_TYPE_M5_FIRE || BOARD_TYPE_M5_BLACK_GRAY
       default 0 if BOARD_TYPE_DEV
+      default 4 if BOARD_TYPE_TTGO_TDISPLAY
     config DISP_INVERT_BKCL_SIGNAL
       bool "Invert backlight signal (on/off)"
       default n

main/process/dashboard.c
@@ -103,6 +103,8 @@ static void reply_version_info(const void* ctx, CborEncoder* container)
     add_string_to_map(&map_encoder, "BOARD_TYPE", "M5FIRE");
 #elif defined(CONFIG_BOARD_TYPE_M5_BLACK_GRAY)
     add_string_to_map(&map_encoder, "BOARD_TYPE", "M5BLACKGRAY");
+#elif defined(CONFIG_BOARD_TYPE_TTGO_TDISPLAY)
+    add_string_to_map(&map_encoder, "BOARD_TYPE", "TTGO_TDISPLAY");
 #elif defined(CONFIG_BOARD_TYPE_DEV)
     add_string_to_map(&map_encoder, "BOARD_TYPE", "DEV");
 #else

main/random.c
@@ -78,7 +78,7 @@ static void get_random_internal(uint8_t* bytes_out, size_t len, const uint8_t* a
 
     // NOTE: there appears to be a GPIO conflict between button-A and the hall
     // sensor on an M5Stack.  Skip trying to read that sensor for extra entropy.
-#if !defined CONFIG_BOARD_TYPE_M5_FIRE && !defined CONFIG_BOARD_TYPE_M5_BLACK_GRAY
+#if !defined(CONFIG_BOARD_TYPE_M5_FIRE) && !defined(CONFIG_BOARD_TYPE_M5_BLACK_GRAY)
     // start first read some data from the hall sensor
     call_uint16_t_func_to_hasher(ctx, hall_sensor_read);
 #endif

commit b0ce1d5d44005fcb9d6c26cf32b113b2137692d3

Fix accidental reversal of m5stack buttons
Fix m5stack regression in 51b6306d1427e6100db0f31a22ef6c3c6c7696c0

main/input.c
@@ -74,24 +74,18 @@ static void button_pressed(void* arg)
     *button = true;
 }
 
-static void button_handle_two_buttonsA(void* arg)
+static void button_released(void* arg)
 {
-    if (button_B_pressed) {
+    // button_A_pressed or button_B_pressed passed in arg to indicate which was released
+    if (button_A_pressed && button_B_pressed) {
         gui_front_click();
-    } else if (button_A_pressed) {
+    } else if (button_A_pressed && arg == &button_A_pressed) {
+        wheel_prev();
+    } else if (button_B_pressed && arg == &button_B_pressed) {
         wheel_next();
     }
-    button_B_pressed = false;
-    button_A_pressed = false;
-}
 
-static void button_handle_two_buttonsB(void* arg)
-{
-    if (button_A_pressed) {
-        gui_front_click();
-    } else if (button_B_pressed) {
-        wheel_prev();
-    }
+    // Clear both flags here so we ignore the second button release when both pressed
     button_B_pressed = false;
     button_A_pressed = false;
 }
@@ -100,11 +94,11 @@ void wheel_init()
 {
     button_handle_t btn_handle_prev = iot_button_create(CONFIG_INPUT_BTN_A, BUTTON_ACTIVE_LOW);
     iot_button_set_evt_cb(btn_handle_prev, BUTTON_CB_PUSH, button_pressed, &button_A_pressed);
-    iot_button_set_evt_cb(btn_handle_prev, BUTTON_CB_RELEASE, button_handle_two_buttonsA, NULL);
+    iot_button_set_evt_cb(btn_handle_prev, BUTTON_CB_RELEASE, button_released, &button_A_pressed);
 
     button_handle_t btn_handle_next = iot_button_create(CONFIG_INPUT_BTN_B, BUTTON_ACTIVE_LOW);
     iot_button_set_evt_cb(btn_handle_next, BUTTON_CB_PUSH, button_pressed, &button_B_pressed);
-    iot_button_set_evt_cb(btn_handle_next, BUTTON_CB_RELEASE, button_handle_two_buttonsB, NULL);
+    iot_button_set_evt_cb(btn_handle_next, BUTTON_CB_RELEASE, button_released, &button_B_pressed);
 }
 
 #else