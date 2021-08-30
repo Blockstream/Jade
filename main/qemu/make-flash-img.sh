4214acfcf3da9b99d66a500c9edb80d8453908a2
Lawrence Nahum <lawrence@greenaddress.it>
2021-08-30 17:46:35 +0200

    use esptool merge_bin functionality to build the flash image

--- main/qemu/make-flash-img.sh
+++ main/qemu/make-flash-img.sh
@@ -2,18 +2,14 @@
 set -e
 
 FLASH_IMAGE_FILE=/qemu/flash_image.bin
-# this is how you create the initial file, however we don't do it at each run
-# because something gets stuck during init which only gets fixed during a reboot
-# and it seems related to the keychain. By reusing the checked in image the
-# keychain is pre-initialized
-# FIXME: fix/investigate keychain nvs init
-# dd if=/dev/zero bs=1024 count=4096 of=${FLASH_IMAGE_FILE}
 
-# using a precreated image
-cp main/qemu/qemu_flash_image.bin ${FLASH_IMAGE_FILE}
-dd if=build/bootloader/bootloader.bin bs=1 seek=$((0x1000)) of=${FLASH_IMAGE_FILE} conv=notrunc
-dd if=build/partition_table/partition-table.bin bs=1 seek=$((0x9000)) of=${FLASH_IMAGE_FILE} conv=notrunc
-dd if=build/ota_data_initial.bin bs=1 seek=$((0xe000)) of=${FLASH_IMAGE_FILE} conv=notrunc
-dd if=build/jade.bin bs=1 seek=$((0x10000)) of=${FLASH_IMAGE_FILE} conv=notrunc
+. /root/esp/esp-idf/export.sh
+
+esptool.py --chip esp32 merge_bin --fill-flash-size 4MB -o ${FLASH_IMAGE_FILE} \
+--flash_mode dio --flash_freq 40m --flash_size 4MB \
+0x9000 build/partition_table/partition-table.bin \
+0xe000 build/ota_data_initial.bin \
+0x1000 build/bootloader/bootloader.bin \
+0x10000 build/jade.bin
 
 cp main/qemu/qemu_efuse.bin /qemu/qemu_efuse.bin
