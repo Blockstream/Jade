072aec4822a03109732831d5ac128a3b1b16db34
Lawrence Nahum <lawrence@greenaddress.it>
2021-08-30 15:08:10 +0200

    add spiram support

--- main/qemu/qemu_reboot.sh
+++ main/qemu/qemu_reboot.sh
@@ -4,6 +4,7 @@ pkill -f qemu-system-xtensa
 
 (cd /qemu && build/qemu-system-xtensa -nographic \
     -machine esp32 \
+    -m 4M \
     -drive file=flash_image.bin,if=mtd,format=raw \
     -global driver=timer.esp32.timg,property=wdt_disable,value=true \
     -nic user,model=open_eth,id=lo0,hostfwd=tcp:0.0.0.0:2222-:2222 \
