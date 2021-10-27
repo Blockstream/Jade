ec30008c468e0c63338103ad823343f2d73ee32e
Jamie C. Driver <jamie@blockstream.io>
2021-10-27 15:24:02 +0100

    Update the button library
    From esp-idf/examples/bluetooth/esp_ble_mesh/common_components/button

--- components/button/button/button.c
+++ components/button/button/button.c
@@ -20,6 +20,7 @@
 #include "driver/gpio.h"
 #include "iot_button.h"
 #include "esp_timer.h"
+#include "esp_rom_sys.h"
 
 #define USE_ESP_TIMER   CONFIG_BUTTON_USE_ESP_TIMER
 #if USE_ESP_TIMER
@@ -200,7 +201,7 @@ static void button_gpio_isr_handler(void* arg)
             #if !USE_ESP_TIMER
             xTimerStopFromISR(btn->tap_psh_cb.tmr, &HPTaskAwoken);
             xTimerResetFromISR(btn->tap_psh_cb.tmr, &HPTaskAwoken);
-            #else 
+            #else
             esp_timer_stop(btn->tap_psh_cb.tmr);
             esp_timer_start_once(btn->tap_psh_cb.tmr, btn->tap_psh_cb.interval * portTICK_PERIOD_MS * 1000);
             #endif
@@ -275,7 +276,7 @@ esp_err_t iot_button_delete(button_handle_t btn_handle)
 button_handle_t iot_button_create(gpio_num_t gpio_num, button_active_t active_level)
 {
     #if USE_ESP_TIMER
-    ets_printf("use esp timer !!!\n");
+    esp_rom_printf("use esp timer !!!\n");
     esp_timer_init();
     #endif
 
