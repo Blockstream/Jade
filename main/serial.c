5d2c5c366853e0e7d5864ac0c5629047e1688f0a
Lawrence Nahum <lawrence@greenaddress.it>
2022-01-16 19:42:07 +0100

    serial: Improve stability/reliability when reading serial data.
    Increase the serial driver rx fifo buffer from 1k to just over 4k
    (sufficient to hold a complete ota_data message).
    Put the serial interrupt handler into IRAM to avoid rx bytes being
    dropped during flash writes.

--- main/serial.c
+++ main/serial.c
@@ -104,7 +104,9 @@ bool serial_init(TaskHandle_t* serial_handle)
     if (err != ESP_OK) {
         return false;
     }
-    err = uart_driver_install(UART_NUM_0, 1024, 1024, 0, NULL, 0);
+
+    /* maximum OTA CHUNK + cbor overhead  for RX */
+    err = uart_driver_install(UART_NUM_0, (1024 * 4) + 46, 1024, 0, NULL, 0);
     if (err != ESP_OK) {
         return false;
     }
