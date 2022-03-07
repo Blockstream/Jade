69364416fa2a825d8f61bb1c697436ac10c80d93
Jamie C. Driver <jamie@blockstream.io>
2022-03-07 13:44:28 +0000

    tools: add script for making delta patches given two compressed firmwares
    Given two compressed firmware images with standard filenames, create two
    compressed patches to 'delta upgrade' between them.  The patch files are
    created with their standard names, in a given output directory.

--- tools/fwtools.py
+++ tools/fwtools.py
@@ -1,8 +1,13 @@
 import zlib
 import logging
 import re
+import os
+
+from collections import namedtuple
+FwInfo = namedtuple('FwInfo', 'version config fwsize')
 
 FWFILE_TYPE_FULL = 'fw.bin'
+FWFILE_TYPE_PATCH = 'patch.bin'
 
 logger = logging.getLogger('jade')
 
@@ -37,6 +42,45 @@ def get_firmware_compressed_filepath(firmware, outputdir):
     filepath = prefix_dir(outputdir, filename)
     logger.info(f'Deduced compressed firmware filepath: {filepath}')
     return filepath
+
+
+# Generate path filename based on versions and ble-configs of from/to fws, the uncompressed
+# size of the final destination firmware, and the uncompressed size of this patch
+# See also 'parse_compressed_filename()' below
+def get_patch_compressed_filepath(patch, frominfo, toinfo, outputdir):
+    # Full file path is:
+    # <dir>/<tover>_<toconfig>_from_<fromver>_<fromcfg>_sizes_<uncompressed-fwsize>_<uncompressed-patchsize>_patch.bin
+    filename = toinfo.version + '_' + toinfo.config + '_from_' + \
+               frominfo.version + '_' + frominfo.config + '_sizes_' + \
+               str(toinfo.fwsize) + '_' + str(len(patch)) + '_' + FWFILE_TYPE_PATCH
+
+    filepath = prefix_dir(outputdir, filename)
+    logger.info(f'Deduced compressed patch filepath: {filepath}')
+
+    return filepath
+
+
+def parse_compressed_filename(filepath):
+    filename = os.path.basename(filepath)
+    parts = filename.split('_')
+
+    if len(parts) == 4 and parts[-1] == FWFILE_TYPE_FULL:
+        # File name is:
+        # <ver>_<config>_<uncompressed-size>_fw.bin[.uncompressed]
+        logger.info(f'Filename suggests full firmware: {filename}')
+        fwinfo = FwInfo(parts[0], parts[1], int(parts[2]))
+        return (FWFILE_TYPE_FULL, fwinfo, None)
+
+    elif len(parts) == 9 and parts[-1] == FWFILE_TYPE_PATCH:
+        # File name is:
+        # <tover>_<otconfig>_from_<fromver>_<fromcfg>_sizes_<uncompressed-fwsize>_<uncompressed-patchsize>_patch.bin
+        logger.info(f'Filename suggests firmware patch: {filename}')
+        toinfo = FwInfo(parts[0], parts[1], int(parts[6]))
+        frominfo = FwInfo(parts[3], parts[4], int(parts[7]))  # store patch len in frominfo size
+        return (FWFILE_TYPE_PATCH, toinfo, frominfo)
+
+    else:
+        raise Exception(f'Unknown filename format: {filename}')
 
 
 # Compress using zlib
---
+++ tools/mkpatch.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+
+import sys
+import logging
+import subprocess
+import os
+
+import fwtools
+
+# Enable logging
+logger = logging.getLogger('jade')
+logger.setLevel(logging.INFO)
+
+
+# Return filepath of binary diff command
+def _bsdiff_cmd():
+    scriptdir = os.path.dirname(os.path.realpath(__file__))
+    bsdiffcmd = fwtools.prefix_dir(scriptdir, 'bsdiff')
+    return bsdiffcmd
+
+
+# Get filepath for a temporary file (in the output dir)
+def _tmpfilepath(outputdir, filename):
+    return fwtools.prefix_dir(outputdir, filename + '.tmp')
+
+
+# Remove file if present - eg. a temporary file
+def _remove(filepath):
+    try:
+        os.remove(filepath)
+    except OSError:
+        pass
+
+
+# Helper to read, decompress (zlib), and write a file
+def write_decompressed(compressedpath, uncompressedpath):
+    compressed = fwtools.read(compressedpath)
+    uncompressed = fwtools.decompress(compressed)
+    return fwtools.write(uncompressed, uncompressedpath)
+
+
+def create_patch(frominfo, frompath, toinfo, topath, outputdir):
+
+    tmppathpatch = _tmpfilepath(outputdir, 'patch')
+    try:
+        # Create the binary diff between uncompressed fw files using bsdiff
+        # (expected to be in same directory as this script)
+        bsdiff = _bsdiff_cmd()
+        rslt = subprocess.run([bsdiff, frompath, topath, tmppathpatch])
+        assert rslt.returncode == 0
+
+        # Read the uncompressed patch data, and write zlib compressed
+        # with the standard expected filename
+        patch = fwtools.read(tmppathpatch)
+        patchpath = fwtools.get_patch_compressed_filepath(patch, frominfo, toinfo, outputdir)
+        compressed = fwtools.compress(patch)
+        fwtools.write(compressed, patchpath)
+    finally:
+        _remove(tmppathpatch)
+
+
+def create_patches(fwpathA, fwpathB, outputdir):
+
+    logger.info(f'Patching between {fwpathA} and {fwpathB}')
+    typeA, infoA, infoA_ = fwtools.parse_compressed_filename(fwpathA)
+    typeB, infoB, infoB_ = fwtools.parse_compressed_filename(fwpathB)
+    assert typeA == fwtools.FWFILE_TYPE_FULL and typeB == fwtools.FWFILE_TYPE_FULL
+    assert infoA_ is None and infoB_ is None
+
+    # Create (temporary) uncompressed fw files
+    tmppathA, tmppathB = _tmpfilepath(outputdir, 'fwA'), _tmpfilepath(outputdir, 'fwB')
+
+    try:
+        # Decompress the source firmware images
+        write_decompressed(fwpathA, tmppathA)
+        write_decompressed(fwpathB, tmppathB)
+
+        # Create patches in both directions
+        create_patch(infoA, tmppathA, infoB, tmppathB, outputdir)
+        create_patch(infoB, tmppathB, infoA, tmppathA, outputdir)
+    finally:
+        # Delete the uncompressed firmware images
+        _remove(tmppathA)
+        _remove(tmppathB)
+
+    logger.info('Done!')
+
+
+# Can be run as a utility
+if __name__ == '__main__':
+    jadehandler = logging.StreamHandler()
+    logger.addHandler(jadehandler)
+
+    assert len(sys.argv) == 4, f'Usage: {sys.argv[0]} fwA fwB output_dir'
+
+    # Compressed firmware (ie. input) files to patch between
+    fwA, fwB, outputdir = sys.argv[1], sys.argv[2], sys.argv[3]
+
+    for fw in [fwA, fwB]:
+        assert os.path.exists(fw) and os.path.isfile(fw), f'Firmware file {fw} not found.'
+    assert os.path.exists(outputdir) and os.path.isdir(
+        outputdir), f'Output directory {outputdir} not found.'
+
+    # Check bsdiff exe has been compiled/is present
+    bsdiff = _bsdiff_cmd()
+    assert os.path.exists(bsdiff) and os.path.isfile(bsdiff), f'{bsdiff} not found.' + \
+        os.linesep + 'Maybe it needs compiling - eg. from top level dir: ' + \
+        os.linesep + 'gcc -O2 -DBSDIFF_EXECUTABLE -o tools/bsdiff components/esp32_bsdiff/bsdiff.c'
+
+    # Create patches between firmware files
+    create_patches(fwA, fwB, outputdir)
