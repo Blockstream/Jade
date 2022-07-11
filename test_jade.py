Blockstream/Jade

commit 0ccf81f1bdfc9744e3a9eea91d1bf44ddfa2a7ce

mnemonic: support scan of qr code of unambiguous mnemonic word prefixes
Rather than insisting any mnemonic qr scanned includes complete valid
mnemonic words, allow a string of mnemonic word prefixes, where each
prefix can be unambiguously expanded into the full mnemonic word.
eg. The first 4 letters of each word (although any length is acceptable,
so long as it constitutes a unique prefix to exactly one word).

main/process/mnemonic.c
@@ -19,6 +19,8 @@
 
 #include <ctype.h>
 
+// NOTE: Jade only supports the bip39 English wordlist
+
 // Should be large enough for all 12 and 24 word mnemonics
 #define MNEMONIC_MAXWORDS 24
 #define MNEMONIC_BUFLEN 256
@@ -73,7 +75,7 @@ static bool mnemonic_new(const size_t nwords, char* mnemonic, const size_t mnemo
     JADE_ASSERT(nwords == 12 || nwords == 24);
     JADE_ASSERT(mnemonic_len == MNEMONIC_BUFLEN);
 
-    // generate and show the mnemonic
+    // Generate and show the mnemonic - NOTE: only the English wordlist is supported.
     char* new_mnemonic = NULL;
     keychain_get_new_mnemonic(&new_mnemonic, nwords);
     JADE_ASSERT(new_mnemonic);
@@ -347,6 +349,7 @@ static bool mnemonic_recover(const size_t nwords, char* mnemonic, const size_t m
     JADE_ASSERT(nwords == 12 || nwords == 24);
     JADE_ASSERT(mnemonic_len == MNEMONIC_BUFLEN);
 
+    // NOTE: only the English wordlist is supported.
     struct words* wordlist;
     bip39_get_wordlist(NULL, &wordlist);
     size_t mnemonic_offset = 0;
@@ -503,6 +506,65 @@ static bool mnemonic_recover(const size_t nwords, char* mnemonic, const size_t m
     return true;
 }
 
+// Take a nul terminated string of space-separated mnemonic-word prefixes, and populate a string of
+// space-separated full mnemonic words (also nul terminated).
+// Returns true if it works!  Returns false if any of the prefixes are not a prefix for exactly one
+// valid mnemonic word, or if the expanded string is too large for the provided buffer.
+static bool expand_words(
+    char* mnemonic, const size_t mnemonic_len, const struct words* wordlist, const char* mnemonic_word_prefixes)
+{
+    JADE_ASSERT(mnemonic);
+    JADE_ASSERT(mnemonic_len);
+    JADE_ASSERT(wordlist);
+    JADE_ASSERT(mnemonic_word_prefixes);
+
+    size_t write_pos = 0;
+    const char* read_ptr = mnemonic_word_prefixes;
+    const char* end_ptr = read_ptr;
+
+    while (*end_ptr != '\0' && write_pos < mnemonic_len) {
+        // Find the next prefix
+        end_ptr = strchr(read_ptr, ' ');
+        if (!end_ptr) {
+            end_ptr = strchr(read_ptr, '\0');
+        }
+        JADE_ASSERT(end_ptr);
+        JADE_ASSERT(end_ptr > mnemonic_word_prefixes);
+
+        // Lookup prefix in the passed wordlist, ensuring exactly one match
+        size_t possible_match;
+        const size_t nmatches = valid_words(read_ptr, (end_ptr - read_ptr), wordlist, &possible_match, 1);
+        if (nmatches != 1) {
+            JADE_LOGW("%d matches for prefix: %.*s", nmatches, (end_ptr - read_ptr), read_ptr);
+            mnemonic[0] = '\0';
+            return false;
+        }
+
+        char* wordlist_extracted = NULL;
+        bip39_get_word(wordlist, possible_match, &wordlist_extracted);
+        const size_t word_len = strlen(wordlist_extracted);
+        if (write_pos + word_len >= mnemonic_len) {
+            JADE_LOGW("Expanded mnemonic too long");
+            wally_free_string(wordlist_extracted);
+            mnemonic[0] = '\0';
+            return false;
+        }
+
+        // Copy the expanded word into the mnemonic
+        strcpy(mnemonic + write_pos, wordlist_extracted);
+        wally_free_string(wordlist_extracted);
+        write_pos += word_len;
+        mnemonic[write_pos++] = *end_ptr; // ie. space separator or nul terminator
+
+        // Update read pointer to be after the whitespace
+        read_ptr = end_ptr + 1;
+    }
+
+    // Return true if we have successfully consumed all input
+    JADE_ASSERT(write_pos <= mnemonic_len);
+    return *end_ptr == '\0';
+}
+
 static bool mnemonic_qr(char* mnemonic, const size_t mnemonic_len)
 {
     JADE_ASSERT(mnemonic_len == MNEMONIC_BUFLEN);
@@ -540,10 +602,16 @@ static bool mnemonic_qr(char* mnemonic, const size_t mnemonic_len)
         const size_t len = strnlen(camera_data.strdata, sizeof(camera_data.strdata));
         JADE_ASSERT(len < sizeof(camera_data.strdata));
 
-        if (len < MNEMONIC_BUFLEN) {
-            strcpy(mnemonic, camera_data.strdata);
-        } else {
+        if (len >= MNEMONIC_BUFLEN) {
             JADE_LOGW("String data from qr unexpectedly long - ignored: %u", len);
+            mnemonic[0] = '\0';
+        }
+
+        // NOTE: only the English wordlist is supported.
+        struct words* wordlist;
+        bip39_get_wordlist(NULL, &wordlist);
+        if (!expand_words(mnemonic, mnemonic_len, wordlist, camera_data.strdata)) {
+            JADE_LOGW("Failed to expand given word prefixes into valid mnemonic words");
             mnemonic[0] = '\0';
         }
     }

commit 250742cd9176e19ed83bb109051a72ad1430fa2f

bip39: fix qr scan where a word is also a prefix to another valid word
Fix issue where a mnemonic qr-code scan is rejected as invalid because
it contains a valid bip39 word which is a prefix to another valid bip39
word - eg. pen, ski, use.

Fixes bug introduced in 0ccf81f1bdfc9744e3a9eea91d1bf44ddfa2a7ce

main/process/debug_set_mnemonic.c
@@ -1,4 +1,5 @@
 #include "../jade_assert.h"
+#include "../jade_wally_verify.h"
 #include "../keychain.h"
 #include "../process.h"
 #include "../sensitive.h"
@@ -14,6 +15,11 @@
 #define NULLSTRING 1
 #define MAX_MNEMONIC_LEN (LONGEST_WORD * NUM_OF_WORDS + NULLSTRING)
 
+// Function to expand word prefixes into full words used when qr-scanning mnemonic.
+// Called in this code purely to test it (as qr-scanning is not covered by the unit tests).
+bool expand_words(
+    char* mnemonic, size_t mnemonic_len, const struct words* wordlist, const char* mnemonic_word_prefixes);
+
 void debug_set_mnemonic_process(void* process_ptr)
 {
     JADE_LOGI("Starting: %u", xPortGetFreeHeapSize());
@@ -24,8 +30,10 @@ void debug_set_mnemonic_process(void* process_ptr)
 
     GET_MSG_PARAMS(process);
 
-    char mnemonic[MAX_MNEMONIC_LEN];
-    SENSITIVE_PUSH(mnemonic, sizeof(mnemonic));
+    char mnemonic_passed[MAX_MNEMONIC_LEN];
+    SENSITIVE_PUSH(mnemonic_passed, sizeof(mnemonic_passed));
+    char mnemonic_expanded[MAX_MNEMONIC_LEN];
+    SENSITIVE_PUSH(mnemonic_expanded, sizeof(mnemonic_expanded));
     char passphrase[PASSPHRASE_MAX_LEN + 1];
     SENSITIVE_PUSH(passphrase, sizeof(passphrase));
     const char* p_passphrase = NULL;
@@ -43,7 +51,8 @@ void debug_set_mnemonic_process(void* process_ptr)
     if (rpc_has_field_data("seed", &params)) {
         rpc_get_bytes_ptr("seed", &params, &seed, &written);
         if (written != 32 && written != 64) {
-            jade_process_reject_message(process, CBOR_RPC_BAD_PARAMETERS, "Invalid seed length", NULL);
+            jade_process_reject_message(
+                process, CBOR_RPC_BAD_PARAMETERS, "Failed to extract valid seed from parameters", NULL);
             goto cleanup;
         }
         keychain_derive_from_seed(seed, written, &keydata);
@@ -54,13 +63,25 @@ void debug_set_mnemonic_process(void* process_ptr)
     } else {
         // Cannot use rpc_get_string_ptr here unfortunately because the resulting string
         // is not nul terminated and we need a nul terminated string to pass to wally
-        rpc_get_string("mnemonic", sizeof(mnemonic), &params, mnemonic, &written);
+        rpc_get_string("mnemonic", sizeof(mnemonic_passed), &params, mnemonic_passed, &written);
         if (written == 0) {
             jade_process_reject_message(
-                process, CBOR_RPC_BAD_PARAMETERS, "Failed to extract mnemonic or seed from parameters", NULL);
+                process, CBOR_RPC_BAD_PARAMETERS, "Failed to extract mnemonic prefixes from parameters", NULL);
             goto cleanup;
         }
 
+        // Expand word prefixes into full words, as used when qr-scanning mnemonic.
+        // Called in this code purely to test it (as qr-scanning is not covered by the unit tests).
+        // NOTE: only the English wordlist is supported.
+        struct words* wordlist = NULL;
+        JADE_WALLY_VERIFY(bip39_get_wordlist(NULL, &wordlist));
+        if (!expand_words(mnemonic_expanded, sizeof(mnemonic_expanded), wordlist, mnemonic_passed)) {
+            jade_process_reject_message(
+                process, CBOR_RPC_BAD_PARAMETERS, "Failed to expand mnemonic prefixes into full mnemonic words", NULL);
+            goto cleanup;
+        }
+
+        // Any bip39 passphrase
         if (rpc_has_field_data("passphrase", &params)) {
             written = 0;
             rpc_get_string("passphrase", sizeof(passphrase), &params, passphrase, &written);
@@ -72,7 +93,8 @@ void debug_set_mnemonic_process(void* process_ptr)
             p_passphrase = passphrase;
         }
 
-        if (!keychain_derive_from_mnemonic(mnemonic, p_passphrase, &keydata)) {
+        // Derive a keychain from the passed mnemonic and passphrase
+        if (!keychain_derive_from_mnemonic(mnemonic_expanded, p_passphrase, &keydata)) {
             jade_process_reject_message(
                 process, CBOR_RPC_BAD_PARAMETERS, "Failed to derive keychain from mnemonic", NULL);
             goto cleanup;
@@ -90,7 +112,7 @@ void debug_set_mnemonic_process(void* process_ptr)
 
         // We need to cache the root mnemonic entropy as it is this that we will persist
         // encrypted to local flash (requiring a passphrase to derive the wallet master key).
-        keychain_cache_mnemonic_entropy(mnemonic);
+        keychain_cache_mnemonic_entropy(mnemonic_expanded);
     }
 
     jade_process_reply_to_message_ok(process);
@@ -99,6 +121,7 @@ void debug_set_mnemonic_process(void* process_ptr)
 cleanup:
     SENSITIVE_POP(&keydata);
     SENSITIVE_POP(passphrase);
-    SENSITIVE_POP(mnemonic);
+    SENSITIVE_POP(mnemonic_expanded);
+    SENSITIVE_POP(mnemonic_passed);
     return;
 }

main/process/mnemonic.c
@@ -49,7 +49,6 @@ void make_enter_passphrase_screen(
     gui_activity_t** activity_ptr, gui_view_node_t* textboxes[], const size_t textboxes_len);
 void make_confirm_passphrase_screen(gui_activity_t** activity_ptr, const char* passphrase, gui_view_node_t** textbox);
 
-#ifndef CONFIG_DEBUG_UNATTENDED_CI
 // Function to change the mnemonic word separator and provide pointers to
 // the start of the words.  USed when confirming one word at a time.
 static void change_mnemonic_word_separator(char* mnemonic, const size_t len, const char old_separator,
@@ -307,14 +306,17 @@ static void enable_relevant_chars(const char* word, const size_t word_len, const
 }
 
 static size_t valid_words(const char* word, const size_t word_len, const struct words* wordlist,
-    size_t* possible_word_list, const size_t possible_word_list_len)
+    size_t* possible_word_list, const size_t possible_word_list_len, bool* exact_match)
 {
     JADE_ASSERT(word);
     JADE_ASSERT(wordlist);
     JADE_ASSERT(possible_word_list);
+    JADE_ASSERT(possible_word_list_len);
+    JADE_ASSERT(exact_match);
 
     JADE_LOGD("word = %s, word_len = %u", word, word_len);
 
+    *exact_match = false;
     size_t num_possible_words = 0;
     for (size_t i = 0; i < possible_word_list_len; ++i) {
         possible_word_list[i] = 0;
@@ -324,17 +326,28 @@ static size_t valid_words(const char* word, const size_t word_len, const struct 
         char* wordlist_extracted = NULL; // TODO: check strlen(wordlist_extracted)
         JADE_WALLY_VERIFY(bip39_get_word(wordlist, wordlist_index, &wordlist_extracted));
 
+        // Test if passed 'word' is a valid prefix of the wordlist word
         const int32_t res = strncmp(wordlist_extracted, word, word_len);
 
         if (res < 0) {
+            // No there yet, continue to next work
             wally_free_string(wordlist_extracted);
             continue;
         } else if (res > 0) {
+            // Too late - gone past word - may as well abandon
             wally_free_string(wordlist_extracted);
             break;
         }
 
-        // return first possible_word_list_len compatible words
+        // If prefix matches, see if it is an exact match for the entire word
+        // (ie. word lengths are also same)
+        if (wordlist_extracted[word_len] == '\0') {
+            JADE_ASSERT(!num_possible_words); // should only happen on first match ...
+            JADE_ASSERT(!*exact_match); // and so should only happen at most once!
+            *exact_match = true;
+        }
+
+        // Return first possible_word_list_len compatible words
         if (num_possible_words < possible_word_list_len) {
             possible_word_list[num_possible_words] = wordlist_index;
         }
@@ -385,8 +398,9 @@ static bool mnemonic_recover(const size_t nwords, char* mnemonic, const size_t m
         enter->is_active = false;
 
         while (char_index < 16) {
+            bool exact_match = false;
             size_t possible_word_list[10];
-            const size_t possible_words = valid_words(word, char_index, wordlist, possible_word_list, 10);
+            const size_t possible_words = valid_words(word, char_index, wordlist, possible_word_list, 10, &exact_match);
             if (possible_words < 11) {
                 enter->is_active = false;
                 char choose_word_title[16];
@@ -517,7 +531,11 @@ static bool mnemonic_recover(const size_t nwords, char* mnemonic, const size_t m
 // space-separated full mnemonic words (also nul terminated).
 // Returns true if it works!  Returns false if any of the prefixes are not a prefix for exactly one
 // valid mnemonic word, or if the expanded string is too large for the provided buffer.
-static bool expand_words(
+// NOTE: There are a load of three-letter words in the bip39 list that are a) valid words in their
+// own right, and also b) prefixes to other words.
+// eg: bar, barely, bargain, barrel; pen, penalty, pencil; ski, skill, skin, skirt
+// In this case we allow/prefer an exact match even when the word is an prefix of other words.
+bool expand_words(
     char* mnemonic, const size_t mnemonic_len, const struct words* wordlist, const char* mnemonic_word_prefixes)
 {
     JADE_ASSERT(mnemonic);
@@ -540,8 +558,9 @@ static bool expand_words(
 
         // Lookup prefix in the passed wordlist, ensuring exactly one match
         size_t possible_match;
-        const size_t nmatches = valid_words(read_ptr, (end_ptr - read_ptr), wordlist, &possible_match, 1);
-        if (nmatches != 1) {
+        bool exact_match = false;
+        const size_t nmatches = valid_words(read_ptr, (end_ptr - read_ptr), wordlist, &possible_match, 1, &exact_match);
+        if (nmatches != 1 && !exact_match) {
             JADE_LOGW("%d matches for prefix: %.*s", nmatches, (end_ptr - read_ptr), read_ptr);
             mnemonic[0] = '\0';
             return false;
@@ -634,7 +653,6 @@ static bool mnemonic_qr(char* mnemonic, const size_t mnemonic_len)
     return false;
 #endif
 }
-#endif // CONFIG_DEBUG_UNATTENDED_CI
 
 static inline bool ascii_sane(const int32_t c) { return c >= 32 && c < 128; }
 

test_jade.py
@@ -153,6 +153,18 @@ TEST_MNEMONIC = 'fish inner face ginger orchard permit useful method fence \
 kidney chuckle party favorite sunset draw limb science crane oval letter \
 slot invite sadness banana'
 
+TEST_MNEMONIC_PREFIXES = 'fish inne face gin orc perm usef meth fen kidn chuc \
+part fav suns draw limb scie cran ova let slot invi sadn bana'
+
+# 'can' and 'net' are ambiguous prefixes, but are an exact match to words in
+# the bip39-wordlist, so should be recognised/allowed.
+TEST_MNEMONIC_PREFIXES_EXACT_MATCH = 'recy wear club hurr indu floa cust gua \
+ae plan scan carr elec reco acco stoc insp net ups can opt brie guid priv'
+
+# One word (met) prefix is not unambiguous: met => metal, method
+TEST_MNEMONIC_PREFIXES_AMBIGUOUS = 'fish inne face gin orc perm usef met fen \
+kidn chuc part fav suns draw limb scie cran ova let slot invi sadn bana'
+
 TEST_MNEMONIC_12 = 'retire verb human ecology best member fiction measure \
 demand stereo wedding olive'
 
@@ -1513,40 +1525,69 @@ ddab03ecc4ae0b5e77c4fc0e5cf6c95a0100000000000f4240000000000000')
         _test_bad_params(jade, badinput, errormsg)
 
 
+def _set_wallet(jade, mnemonic=TEST_MNEMONIC, passphrase=None):
+    # Set mnemonic
+    request = jade.build_request("id_mnem", "debug_set_mnemonic",
+                                 {"mnemonic": mnemonic, "passphrase": passphrase})
+    reply = jade.make_rpc_call(request)
+    assert reply['id'] == request['id']
+    assert 'error' not in reply
+    assert reply['result'] is True
+
+    # Get and return root xpub
+    request = jade.build_request("id_xpub", "get_xpub",
+                                 {"network": "mainnet", "path": []})
+    reply = jade.make_rpc_call(request)
+    assert reply['id'] == request['id']
+    assert 'error' not in reply
+    assert reply['result'].startswith('xpub')
+    return reply['result']
+
+
+def test_mnemonic_prefixes(jade):
+
+    # Check the mnemonic unique prefixes expands to the same mnemonic/wallet
+    # as when giving the full mnemonic words (test for qr-scanning prefixes)
+    # as the unambiguous prefixes are expanded to the full words.  orc -> orchard
+    xpub_root0 = _set_wallet(jade, mnemonic=TEST_MNEMONIC)
+    xpub_root1 = _set_wallet(jade, mnemonic=TEST_MNEMONIC_PREFIXES)
+    assert xpub_root1 == xpub_root0
+
+    # Check that mnemonic-prefixes are accepted even if they are prefixes to multiple
+    # words, provided one of them is an exact/full match for the entire word.
+    # eg. 'pen' is a prefix to 'pen', 'penalty' and 'pencil' - but is accepted as it
+    # is an exact full match for 'pen', so no 'expansion' is carried out.  pen -> pen.
+    xpub_root2 = _set_wallet(jade, mnemonic=TEST_MNEMONIC_PREFIXES_EXACT_MATCH)
+    assert xpub_root2 != xpub_root0
+
+
+def test_mnemonic_prefixes_bad(jade):
+    # Check that mnemonic-prefixes are rejected if the prefixes match multiple words
+    # (but none of them exactly/full-match).  ie. prefix is ambiguous.  met -> metal, method
+    for bad_prefixes in [TEST_MNEMONIC_PREFIXES_AMBIGUOUS]:
+        request = jade.build_request("badprefix_" + bad_prefixes[0:4], "debug_set_mnemonic",
+                                     {"mnemonic": bad_prefixes})
+        reply = jade.make_rpc_call(request)
+        assert reply['id'] == request['id']
+        assert 'result' not in reply
+        assert reply['error']['code'] == JadeError.BAD_PARAMETERS
+        assert reply['error']['message'].startswith('Failed to expand mnemonic prefixes')
+
+
 def test_passphrase(jade):
-    def _set_wallet(passphrase=None):
-        # Set mnemonic
-        request = jade.build_request("id_mnem", "debug_set_mnemonic",
-                                     {"mnemonic": TEST_MNEMONIC,
-                                      "passphrase": passphrase})
-        reply = jade.make_rpc_call(request)
-        assert reply['id'] == request['id']
-        assert 'error' not in reply
-        assert reply['result'] is True
-
-        # Get root xpub
-        request = jade.build_request("id_xpub", "get_xpub",
-                                     {"network": "mainnet",
-                                      "path": []})
-        reply = jade.make_rpc_call(request)
-        assert reply['id'] == request['id']
-        assert 'error' not in reply
-        assert reply['result'].startswith('xpub')
-        return reply['result']
-
     # Set mnemonic with/without a passphrase, and get root xpub
-    xpub0 = _set_wallet(passphrase=None)
-    xpub1 = _set_wallet(passphrase="Passphrase1")
-    xpub2 = _set_wallet(passphrase="Passphrase2")
+    xpub0 = _set_wallet(jade, passphrase=None)
+    xpub1 = _set_wallet(jade, passphrase="Passphrase1")
+    xpub2 = _set_wallet(jade, passphrase="Passphrase2")
 
     # Check root xpubs are not the same
     # ie. that the passphrase leads to a different wallet
     assert xpub0 != xpub1 and xpub1 != xpub2 and xpub2 != xpub0
 
     # Check that using the same passphrase does get the same wallet
-    xpub0_again = _set_wallet(passphrase=None)
-    xpub1_again = _set_wallet(passphrase="Passphrase1")
-    xpub2_again = _set_wallet(passphrase="Passphrase2")
+    xpub0_again = _set_wallet(jade, passphrase=None)
+    xpub1_again = _set_wallet(jade, passphrase="Passphrase1")
+    xpub2_again = _set_wallet(jade, passphrase="Passphrase2")
 
     assert xpub0_again == xpub0 and xpub1_again == xpub1 and xpub2_again == xpub2
 
@@ -2465,6 +2506,10 @@ def run_interface_tests(jadeapi,
         # Test good pinserver handshake, and also 'bad sig' pinserver
         test_handshake(jadeapi.jade)
         test_handshake_bad_sig(jadeapi.jade)
+
+        # Test expanding mnemonic words from unique prefixes
+        test_mnemonic_prefixes(jadeapi.jade)
+        test_mnemonic_prefixes_bad(jadeapi.jade)
 
         # Test mnemonic-with-passphrase
         test_passphrase(jadeapi.jade)