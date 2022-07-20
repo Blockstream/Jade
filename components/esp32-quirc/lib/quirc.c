9e65493b7f79c3fcb8e5b42d69e6d649ec92ef06
Jamie C. Driver <jamie@blockstream.io>
2022-07-20 16:41:52 +0100

    quirc: update quirc library to the version from ESP32QRCodeReader
    Copy components/esp32-quirc unchanged from ESP32QRCodeReader
    github.com:alvarowolfx/ESP32QRCodeReader.git at master commit
    0799a7a2ba0528f0be1e7033e7e8c883df128842
    
    NOTE: this commit does not compile!
    
    Will apply tweaks to integrate to our build as following commit, but
    this version of the lib is expected to be faster on esp32.

--- components/esp32-quirc/lib/quirc.c
+++ components/esp32-quirc/lib/quirc.c
@@ -17,158 +17,88 @@
 #include <stdlib.h>
 #include <string.h>
 #include "quirc_internal.h"
-
-/* NOTE: TWEAK FOR ESP32 TO PREFER SPIRAM FOR IMAGE BUFFER */
-#include "esp_heap_caps.h"
+#include <Arduino.h>
 
 const char *quirc_version(void)
 {
-	return "1.0";
+  return "1.0";
 }
 
+//static struct quirc _q;
 struct quirc *quirc_new(void)
 {
-	struct quirc *q = malloc(sizeof(*q));
+  struct quirc *q = ps_malloc(sizeof(*q));
 
-	if (!q)
-		return NULL;
+  if (!q)
+    return NULL;
 
-	memset(q, 0, sizeof(*q));
-	return q;
+  memset(q, 0, sizeof(*q));
+  return q;
 }
 
 void quirc_destroy(struct quirc *q)
 {
-	free(q->image);
-	/* q->pixels may alias q->image when their type representation is of the
-	   same size, so we need to be careful here to avoid a double free */
-	if (!QUIRC_PIXEL_ALIAS_IMAGE)
-		free(q->pixels);
-	free(q->flood_fill_vars);
-	free(q);
+  if (q->image)
+    if (q->image)
+      free(q->image);
+  if (sizeof(*q->image) != sizeof(*q->pixels))
+    if (q->pixels)
+      free(q->pixels);
+
+  if (q)
+    free(q);
 }
 
+//static quirc_pixel_t img_buf[320*240];
 int quirc_resize(struct quirc *q, int w, int h)
 {
-	uint8_t		*image  = NULL;
-	quirc_pixel_t	*pixels = NULL;
-	size_t num_vars;
-	size_t vars_byte_size;
-	struct quirc_flood_fill_vars *vars = NULL;
+  if (q->image)
+  {
+    free(q->image);
+  }
+  uint8_t *new_image = ps_malloc(w * h);
 
-	/*
-	 * XXX: w and h should be size_t (or at least unsigned) as negatives
-	 * values would not make much sense. The downside is that it would break
-	 * both the API and ABI. Thus, at the moment, let's just do a sanity
-	 * check.
-	 */
-	if (w < 0 || h < 0)
-		goto fail;
+  if (!new_image)
+    return -1;
 
-	/*
-	 * alloc a new buffer for q->image. We avoid realloc(3) because we want
-	 * on failure to be leave `q` in a consistant, unmodified state.
-	 */
-	/* NOTE: TWEAK FOR ESP32 TO PREFER SPIRAM FOR IMAGE BUFFER */
-	//image = calloc(w, h);
-	image = heap_caps_malloc(w * h, MALLOC_CAP_SPIRAM | MALLOC_CAP_8BIT);
-	if (!image)
-		goto fail;
-        memset(image, 0, w * h);
-
-	/* compute the "old" (i.e. currently allocated) and the "new"
-	   (i.e. requested) image dimensions */
-	size_t olddim = q->w * q->h;
-	size_t newdim = w * h;
-	size_t min = (olddim < newdim ? olddim : newdim);
-
-	/*
-	 * copy the data into the new buffer, avoiding (a) to read beyond the
-	 * old buffer when the new size is greater and (b) to write beyond the
-	 * new buffer when the new size is smaller, hence the min computation.
-	 */
-	(void)memcpy(image, q->image, min);
-
-	/* alloc a new buffer for q->pixels if needed */
-	if (!QUIRC_PIXEL_ALIAS_IMAGE) {
-	        /* NOTE: TWEAK FOR ESP32 TO PREFER SPIRAM FOR IMAGE BUFFER */
-		//pixels = calloc(newdim, sizeof(quirc_pixel_t));
-		pixels = heap_caps_malloc(newdim * sizeof(quirc_pixel_t), MALLOC_CAP_SPIRAM | MALLOC_CAP_8BIT);
-		if (!pixels)
-			goto fail;
-                memset(pixels, 0, newdim * sizeof(quirc_pixel_t));
-	}
-
-	/*
-	 * alloc the work area for the flood filling logic.
-	 *
-	 * the size was chosen with the following assumptions and observations:
-	 *
-	 * - rings are the regions which requires the biggest work area.
-	 * - they consumes the most when they are rotated by about 45 degree.
-	 *   in that case, the necessary depth is about (2 * height_of_the_ring).
-	 * - the maximum height of rings would be about 1/3 of the image height.
-	 */
-
-	if ((size_t)h * 2 / 2 != h) {
-		goto fail; /* size_t overflow */
-	}
-	num_vars = (size_t)h * 2 / 3;
-	if (num_vars == 0) {
-		num_vars = 1;
-	}
-
-	vars_byte_size = sizeof(*vars) * num_vars;
-	if (vars_byte_size / sizeof(*vars) != num_vars) {
-		goto fail; /* size_t overflow */
-	}
-	vars = malloc(vars_byte_size);
-	if (!vars)
-		goto fail;
-
-	/* alloc succeeded, update `q` with the new size and buffers */
-	q->w = w;
-	q->h = h;
-	free(q->image);
-	q->image = image;
-	if (!QUIRC_PIXEL_ALIAS_IMAGE) {
-		free(q->pixels);
-		q->pixels = pixels;
-	}
-	free(q->flood_fill_vars);
-	q->flood_fill_vars = vars;
-	q->num_flood_fill_vars = num_vars;
-
-	return 0;
-	/* NOTREACHED */
-fail:
-	free(image);
-	free(pixels);
-	free(vars);
-
-	return -1;
+  if (sizeof(*q->image) != sizeof(*q->pixels))
+  { //should gray, 1==1
+    size_t new_size = w * h * sizeof(quirc_pixel_t);
+    if (q->pixels)
+      free(q->pixels);
+    quirc_pixel_t *new_pixels = ps_malloc(new_size);
+    if (!new_pixels)
+    {
+      free(new_image);
+      return -1;
+    }
+    q->pixels = new_pixels;
+  }
+  q->image = new_image;
+  q->w = w;
+  q->h = h;
+  return 0;
 }
 
 int quirc_count(const struct quirc *q)
 {
-	return q->num_grids;
+  return q->num_grids;
 }
 
 static const char *const error_table[] = {
-	[QUIRC_SUCCESS] = "Success",
-	[QUIRC_ERROR_INVALID_GRID_SIZE] = "Invalid grid size",
-	[QUIRC_ERROR_INVALID_VERSION] = "Invalid version",
-	[QUIRC_ERROR_FORMAT_ECC] = "Format data ECC failure",
-	[QUIRC_ERROR_DATA_ECC] = "ECC failure",
-	[QUIRC_ERROR_UNKNOWN_DATA_TYPE] = "Unknown data type",
-	[QUIRC_ERROR_DATA_OVERFLOW] = "Data overflow",
-	[QUIRC_ERROR_DATA_UNDERFLOW] = "Data underflow"
-};
+    [QUIRC_SUCCESS] = "Success",
+    [QUIRC_ERROR_INVALID_GRID_SIZE] = "Invalid grid size",
+    [QUIRC_ERROR_INVALID_VERSION] = "Invalid version",
+    [QUIRC_ERROR_FORMAT_ECC] = "Format data ECC failure",
+    [QUIRC_ERROR_DATA_ECC] = "ECC failure",
+    [QUIRC_ERROR_UNKNOWN_DATA_TYPE] = "Unknown data type",
+    [QUIRC_ERROR_DATA_OVERFLOW] = "Data overflow",
+    [QUIRC_ERROR_DATA_UNDERFLOW] = "Data underflow"};
 
 const char *quirc_strerror(quirc_decode_error_t err)
 {
-	if (err >= 0 && err < sizeof(error_table) / sizeof(error_table[0]))
-		return error_table[err];
+  if (err >= 0 && err < sizeof(error_table) / sizeof(error_table[0]))
+    return error_table[err];
 
-	return "Unknown error";
+  return "Unknown error";
 }
--- components/esp32-quirc/lib/quirc_internal.h
+++ components/esp32-quirc/lib/quirc_internal.h
@@ -17,112 +17,104 @@
 #ifndef QUIRC_INTERNAL_H_
 #define QUIRC_INTERNAL_H_
 
-#include <assert.h>
-#include <stdlib.h>
-
 #include "quirc.h"
 
-#define QUIRC_ASSERT(a)	assert(a)
-
-#define QUIRC_PIXEL_WHITE	0
-#define QUIRC_PIXEL_BLACK	1
-#define QUIRC_PIXEL_REGION	2
+#define QUIRC_PIXEL_WHITE 0
+#define QUIRC_PIXEL_BLACK 1
+#define QUIRC_PIXEL_REGION 2
 
 #ifndef QUIRC_MAX_REGIONS
-#define QUIRC_MAX_REGIONS	254
+#define QUIRC_MAX_REGIONS 254
 #endif
-#define QUIRC_MAX_CAPSTONES	32
-#define QUIRC_MAX_GRIDS		(QUIRC_MAX_CAPSTONES * 2)
 
-#define QUIRC_PERSPECTIVE_PARAMS	8
+#define QUIRC_MAX_CAPSTONES 32
+#define QUIRC_MAX_GRIDS 8
+
+#define QUIRC_PERSPECTIVE_PARAMS 8
 
 #if QUIRC_MAX_REGIONS < UINT8_MAX
-#define QUIRC_PIXEL_ALIAS_IMAGE	1
 typedef uint8_t quirc_pixel_t;
 #elif QUIRC_MAX_REGIONS < UINT16_MAX
-#define QUIRC_PIXEL_ALIAS_IMAGE	0
 typedef uint16_t quirc_pixel_t;
 #else
 #error "QUIRC_MAX_REGIONS > 65534 is not supported"
 #endif
 
-struct quirc_region {
-	struct quirc_point	seed;
-	int			count;
-	int			capstone;
-};
+struct quirc_region
+{
+  struct quirc_point seed;
+  int count;
+  int capstone;
+} __attribute__((aligned(8)));
 
-struct quirc_capstone {
-	int			ring;
-	int			stone;
+struct quirc_capstone
+{
+  int ring;
+  int stone;
 
-	struct quirc_point	corners[4];
-	struct quirc_point	center;
-	double			c[QUIRC_PERSPECTIVE_PARAMS];
+  struct quirc_point corners[4];
+  struct quirc_point center;
+  float c[QUIRC_PERSPECTIVE_PARAMS];
 
-	int			qr_grid;
-};
+  int qr_grid;
+} __attribute__((aligned(8)));
 
-struct quirc_grid {
-	/* Capstone indices */
-	int			caps[3];
+struct quirc_grid
+{
+  /* Capstone indices */
+  int caps[3];
 
-	/* Alignment pattern region and corner */
-	int			align_region;
-	struct quirc_point	align;
+  /* Alignment pattern region and corner */
+  int align_region;
+  struct quirc_point align;
 
-	/* Timing pattern endpoints */
-	struct quirc_point	tpep[3];
+  /* Timing pattern endpoints */
+  struct quirc_point tpep[3];
+  int hscan;
+  int vscan;
 
-	/* Grid size and perspective transform */
-	int			grid_size;
-	double			c[QUIRC_PERSPECTIVE_PARAMS];
-};
+  /* Grid size and perspective transform */
+  int grid_size;
+  float c[QUIRC_PERSPECTIVE_PARAMS];
+} __attribute__((aligned(8)));
 
-struct quirc_flood_fill_vars {
-	int y;
-	int right;
-	int left_up;
-	int left_down;
-};
+struct quirc
+{
+  uint8_t *image;
+  quirc_pixel_t *pixels;
+  int w;
+  int h;
 
-struct quirc {
-	uint8_t			*image;
-	quirc_pixel_t		*pixels;
-	int			w;
-	int			h;
+  int num_regions;
+  struct quirc_region regions[QUIRC_MAX_REGIONS];
 
-	int			num_regions;
-	struct quirc_region	regions[QUIRC_MAX_REGIONS];
+  int num_capstones;
+  struct quirc_capstone capstones[QUIRC_MAX_CAPSTONES];
 
-	int			num_capstones;
-	struct quirc_capstone	capstones[QUIRC_MAX_CAPSTONES];
-
-	int			num_grids;
-	struct quirc_grid	grids[QUIRC_MAX_GRIDS];
-
-	size_t      		num_flood_fill_vars;
-	struct quirc_flood_fill_vars *flood_fill_vars;
-};
+  int num_grids;
+  struct quirc_grid grids[QUIRC_MAX_GRIDS];
+} __attribute__((aligned(8)));
 
 /************************************************************************
  * QR-code version information database
  */
 
-#define QUIRC_MAX_VERSION     40
-#define QUIRC_MAX_ALIGNMENT   7
+#define QUIRC_MAX_VERSION 40
+#define QUIRC_MAX_ALIGNMENT 7
 
-struct quirc_rs_params {
-	int             bs; /* Small block size */
-	int             dw; /* Small data words */
-	int		ns; /* Number of small blocks */
-};
+struct quirc_rs_params
+{
+  uint8_t bs; /* Small block size */
+  uint8_t dw; /* Small data words */
+  uint8_t ns; /* Number of small blocks */
+} __attribute__((aligned(8)));
 
-struct quirc_version_info {
-	int				data_bytes;
-	int				apat[QUIRC_MAX_ALIGNMENT];
-	struct quirc_rs_params          ecc[4];
-};
+struct quirc_version_info
+{
+  uint16_t data_bytes;
+  uint8_t apat[QUIRC_MAX_ALIGNMENT];
+  struct quirc_rs_params ecc[4];
+} __attribute__((aligned(8)));
 
 extern const struct quirc_version_info quirc_version_db[QUIRC_MAX_VERSION + 1];
 
