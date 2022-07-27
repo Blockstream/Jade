3cbc259ba6241851979a7608fe800ccded782d89
Lawrence Nahum <lawrence@greenaddress.it>
2022-07-27 21:41:44 +0200

    security: wrap abort to jade_abort

--- main/jade_abort.c
+++ main/jade_abort.c
@@ -9,6 +9,8 @@
 
 #define SIZE_OF_ERR_BUFF 128
 
+extern void __real_abort(void);
+
 void jade_abort(const char* file, const int line_n)
 {
     // Clear senstitive data
@@ -29,5 +31,11 @@ void jade_abort(const char* file, const int line_n)
     // 1) for the user to see the "Internal error" message
     // 2) to give serial writer a chance to process outstanding logging
     vTaskDelay(5000 / portTICK_PERIOD_MS);
-    abort();
+    __real_abort();
 }
+
+// we wrap the real abort in the entire firmwawre so that ours gets called instead
+// (see CMakeLists.txt) which will in turn call the real abort
+// we do this so that we can clear the keychain and the sensitive stack
+
+void __wrap_abort(void) { jade_abort("WRAPPED", 0); }
