5b0005922f0523d8dbe9b2ebb0f5ea6a868bd6d9
Jamie C. Driver <jamie@blockstream.io>
2022-09-05 15:50:29 +0100

    qr: fix size/version of qr code used for displaying xpub string

--- main/process/dashboard.c
+++ main/process/dashboard.c
@@ -1181,18 +1181,26 @@ static void handle_storage(void)
 #ifdef CONFIG_DEBUG_MODE
 static void handle_xpub(void)
 {
+    // Version 6 is required as xpubs strings are mixed case and up to 112 chars long.
+    // Version 6 is up to 134 characters ('binary' mode, as 'alphanumeric' mode only supports uppercase)
+    // See: https://www.qrcode.com/en/about/version.html
+    const uint8_t qrcode_version = 6;
+
+    // A v6 qrcode is 41x41 - we can scale this by 3 and it still (just) fits on the display
+    const size_t scale_factor = 3;
+
     const char* network = keychain_get_network_type_restriction() == NETWORK_TYPE_TEST ? "testnet" : "mainnet";
     char* xpub = NULL;
     if (keychain_get() && wallet_get_xpub(network, NULL, 0, &xpub)) {
-        QRCode qrcode;
         Icon qr_icon;
+        QRCode qrcode;
+        uint8_t qrbuffer[256]; // underlying qrcode data/work area - opaque
+        JADE_ASSERT(sizeof(qrbuffer) > qrcode_getBufferSize(qrcode_version));
 
-        uint8_t* qrcodeBytes = JADE_CALLOC(qrcode_getBufferSize(4), sizeof(uint8_t));
-        const int qret = qrcode_initText(&qrcode, qrcodeBytes, 4, ECC_LOW, xpub);
+        const int qret = qrcode_initText(&qrcode, qrbuffer, qrcode_version, ECC_LOW, xpub);
         JADE_ASSERT(qret == 0);
-        qrcode_toIcon(&qrcode, &qr_icon, 3);
+        qrcode_toIcon(&qrcode, &qr_icon, scale_factor);
         wally_free_string(xpub);
-        free(qrcodeBytes);
 
         display_icon_activity(&qr_icon, TFT_LIGHTGREY, NULL, NULL);
         sync_await_single_event(GUI_EVENT, GUI_FRONT_CLICK_EVENT, NULL, NULL, NULL, 0);
