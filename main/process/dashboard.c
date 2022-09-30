Blockstream/Jade

commit 893ad81e22818da0395992ecfdbaed64cdae9e0f

gui: create all four dashboard screens as 'unmanaged' activities
Explicitly manage the lifetime of the dashboard screens rather than
having them in the list of managed/outstanding  activities - this
makes it possible to free the list of managed activities from within any
workflow, as the top-level dashboard screens will not be included in
the list of activities freed.

main/process/dashboard.c
@@ -1414,9 +1414,8 @@ void dashboard_process(void* process_ptr)
     JADE_ASSERT(running);
     const esp_err_t err = esp_ota_get_partition_description(running, &running_app_info);
     JADE_ASSERT(err == ESP_OK);
-
     wait_event_data_t* const event_data = make_wait_event_data();
-    gui_activity_t* act_dashboard = NULL;
+    JADE_ASSERT(event_data);
 
     // NOTE: Create 'Ready' screen for when Jade is unlocked and ready to use early, so that
     // it does not fragment the RAM (since it is long-lived once unit is unlocked with PIN).
@@ -1440,7 +1439,10 @@ void dashboard_process(void* process_ptr)
         //    - connect screen
         // 4. Uninitialised - has no persisted/encrypted keys and no keys in memory
         //    - setup screen
-        act_dashboard = NULL;
+        // NOTE: All dashboard screens are created as 'unmanaged' activities, so are not placed
+        // in the list of activities to be freed by 'set_current_activity_ex()' calls, so any
+        // 'act_dashboard' created here must be explicitly freed when no longer relevant.
+        gui_activity_t* act_dashboard = NULL;
         const bool has_pin = keychain_has_pin();
         const keychain_t* initial_keychain = keychain_get();
         if (initial_keychain && keychain_get_userdata() != SOURCE_NONE) {
@@ -1465,5 +1467,10 @@ void dashboard_process(void* process_ptr)
         // NOTE: connecting or disconnecting serial or ble will cause any keys to
         // be cleared (and bzero'd).
         do_dashboard(process, initial_keychain, has_pin, act_dashboard, event_data);
+
+        // Free the dashboard (assuming it isn't the 'Ready' screen)
+        if (act_dashboard != act_ready) {
+            free_unmanaged_activity(act_dashboard);
+        }
     }
 }

main/ui/dashboard.c
@@ -31,6 +31,9 @@ void make_startup_options_screen(gui_activity_t** activity_ptr)
     add_buttons((*activity_ptr)->root_node, UI_COLUMN, btns, 4);
 }
 
+// NOTE: This 'dashboard' screen is created as an 'unmanaged' activity, so it is not placed
+// in the list of activities to be freed by 'set_current_activity_ex()' calls.
+// It must be freed by the caller.
 void make_setup_screen(gui_activity_t** activity_ptr, const char* device_name, const char* firmware_version)
 {
     JADE_ASSERT(activity_ptr);
@@ -40,7 +43,8 @@ void make_setup_screen(gui_activity_t** activity_ptr, const char* device_name, c
     const int ret = snprintf(title, sizeof(title), "Setup %s", device_name);
     JADE_ASSERT(ret > 0 && ret < sizeof(title));
 
-    gui_make_activity(activity_ptr, true, title);
+    // NOTE: This 'dashboard' screen is created as an 'unmanaged' activity
+    gui_make_activity_ex(activity_ptr, true, title, false);
 
     gui_view_node_t* vsplit;
     gui_make_vsplit(&vsplit, GUI_SPLIT_RELATIVE, 3, 32, 52, 16);
@@ -75,6 +79,9 @@ void make_setup_screen(gui_activity_t** activity_ptr, const char* device_name, c
     }
 }
 
+// NOTE: This 'dashboard' screen is created as an 'unmanaged' activity, so it is not placed
+// in the list of activities to be freed by 'set_current_activity_ex()' calls.
+// It must be freed by the caller.
 void make_connect_screen(gui_activity_t** activity_ptr, const char* device_name, const char* firmware_version)
 {
     JADE_ASSERT(activity_ptr);
@@ -85,7 +92,8 @@ void make_connect_screen(gui_activity_t** activity_ptr, const char* device_name,
     const int ret = snprintf(title, sizeof(title), "Connect %s", device_name);
     JADE_ASSERT(ret > 0 && ret < sizeof(title));
 
-    gui_make_activity(activity_ptr, true, title);
+    // NOTE: This 'dashboard' screen is created as an 'unmanaged' activity
+    gui_make_activity_ex(activity_ptr, true, title, false);
 
     gui_view_node_t* vsplit;
     gui_make_vsplit(&vsplit, GUI_SPLIT_RELATIVE, 3, 45, 40, 15);
@@ -140,6 +148,9 @@ void make_connection_select_screen(gui_activity_t** activity_ptr)
 }
 #endif // CONFIG_ESP32_NO_BLOBS
 
+// NOTE: This 'dashboard' screen is created as an 'unmanaged' activity, so it is not placed
+// in the list of activities to be freed by 'set_current_activity_ex()' calls.
+// It must be freed by the caller.
 void make_connect_to_screen(gui_activity_t** activity_ptr, const char* device_name, const bool ble)
 {
     JADE_ASSERT(activity_ptr);
@@ -149,7 +160,8 @@ void make_connect_to_screen(gui_activity_t** activity_ptr, const char* device_na
     const int ret = snprintf(title, sizeof(title), "Connect %s", device_name);
     JADE_ASSERT(ret > 0 && ret < sizeof(title));
 
-    gui_make_activity(activity_ptr, true, title);
+    // NOTE: This 'dashboard' screen is created as an 'unmanaged' activity
+    gui_make_activity_ex(activity_ptr, true, title, false);
 
     gui_view_node_t* vsplit;
     gui_make_vsplit(&vsplit, GUI_SPLIT_RELATIVE, 2, 60, 40);
@@ -180,14 +192,15 @@ void make_connect_to_screen(gui_activity_t** activity_ptr, const char* device_na
 #endif
 }
 
+// NOTE: The main 'Ready' screen is created as an 'unmanaged' activity, so it is not placed
+// in the list of activities to be freed by 'set_current_activity_ex()' calls.
+// This is becase the 'Ready' screen is never freed and lives as long as the application itself.
 void make_ready_screen(gui_activity_t** activity_ptr, const char* device_name, gui_view_node_t** txt_extra)
 {
     JADE_ASSERT(activity_ptr);
     JADE_ASSERT(device_name);
 
-    // NOTE: The main 'Ready' screen is created as an 'unmanaged' activity, so it is not placed
-    // in the list of activities to be freed by 'set_current_activity_ex()' calls.
-    // This is becase the 'Ready' screen is never freed and lives as long as the application itself.
+    // NOTE: This 'dashboard' screen is created as an 'unmanaged' activity
     gui_make_activity_ex(activity_ptr, true, device_name, false);
 
     gui_view_node_t* vsplit;

commit c9942938dbedda1778d5fe91f673c3313a273278

dashboard: fix internal error when using 'Recovery Phrase Login'
Fixes regression introduced in in:
893ad81e22818da0395992ecfdbaed64cdae9e0f

main/process/dashboard.c
@@ -1439,9 +1439,11 @@ void dashboard_process(void* process_ptr)
         //    - connect screen
         // 4. Uninitialised - has no persisted/encrypted keys and no keys in memory
         //    - setup screen
-        // NOTE: All dashboard screens are created as 'unmanaged' activities, so are not placed
+        // NOTE: Some dashboard screens are created as 'unmanaged' activities, so are not placed
         // in the list of activities to be freed by 'set_current_activity_ex()' calls, so any
         // 'act_dashboard' created here must be explicitly freed when no longer relevant.
+        // 'free_dashboard' is set when this is the case.
+        bool free_dashboard = false;
         gui_activity_t* act_dashboard = NULL;
         const bool has_pin = keychain_has_pin();
         const keychain_t* initial_keychain = keychain_get();
@@ -1450,15 +1452,19 @@ void dashboard_process(void* process_ptr)
             const char* additional = keychain_has_temporary() ? "(Temporary Wallet)" : "";
             gui_update_text(txt_extra, additional);
             act_dashboard = act_ready;
+            // free_dashboard is not required as this screen lives for the lifetime of the application
         } else if (initial_keychain) {
             JADE_LOGI("Wallet/keys initialised but not yet saved - showing Connect-To screen");
             MAKE_DASHBOARD_SCREEN(make_connect_to_screen, act_dashboard, initialisation_via_ble);
+            // free_dashboard is not required as this is a standard 'managed' activity
         } else if (has_pin) {
             JADE_LOGI("Wallet/keys pin set but not yet loaded - showing Connect screen");
             MAKE_DASHBOARD_SCREEN(make_connect_screen, act_dashboard, running_app_info.version);
+            free_dashboard = true;
         } else {
             JADE_LOGI("No wallet/keys and no pin set - showing Setup screen");
             MAKE_DASHBOARD_SCREEN(make_setup_screen, act_dashboard, running_app_info.version);
+            free_dashboard = true;
         }
 
         // This call loops/blocks all the time the user keychain (and related details)
@@ -1468,8 +1474,8 @@ void dashboard_process(void* process_ptr)
         // be cleared (and bzero'd).
         do_dashboard(process, initial_keychain, has_pin, act_dashboard, event_data);
 
-        // Free the dashboard (assuming it isn't the 'Ready' screen)
-        if (act_dashboard != act_ready) {
+        // Free any dashboard screen if flagged as needing explicit free
+        if (free_dashboard) {
             free_unmanaged_activity(act_dashboard);
         }
     }

main/ui/dashboard.c
@@ -148,9 +148,6 @@ void make_connection_select_screen(gui_activity_t** activity_ptr)
 }
 #endif // CONFIG_ESP32_NO_BLOBS
 
-// NOTE: This 'dashboard' screen is created as an 'unmanaged' activity, so it is not placed
-// in the list of activities to be freed by 'set_current_activity_ex()' calls.
-// It must be freed by the caller.
 void make_connect_to_screen(gui_activity_t** activity_ptr, const char* device_name, const bool ble)
 {
     JADE_ASSERT(activity_ptr);
@@ -160,8 +157,7 @@ void make_connect_to_screen(gui_activity_t** activity_ptr, const char* device_na
     const int ret = snprintf(title, sizeof(title), "Connect %s", device_name);
     JADE_ASSERT(ret > 0 && ret < sizeof(title));
 
-    // NOTE: This 'dashboard' screen is created as an 'unmanaged' activity
-    gui_make_activity_ex(activity_ptr, true, title, false);
+    gui_make_activity(activity_ptr, true, title);
 
     gui_view_node_t* vsplit;
     gui_make_vsplit(&vsplit, GUI_SPLIT_RELATIVE, 2, 60, 40);