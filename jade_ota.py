Blockstream/Jade

commit 5f280bd363e2ade22564d74eb889d01e4344e0cd

scripts: check fw name widths to better handle alpha/beta builds

update_jade_fw.py
@@ -30,8 +30,8 @@ def get_fw_metadata(verinfo, release_data):
     def _full_fw_label(fw):
         return f'{fw["version"]} - {fw["config"]}'
 
-    def _delta_fw_label(fw):
-        return _full_fw_label(fw).ljust(18) + f'FROM  {fw["from_version"]} - {fw["from_config"]}'
+    def _delta_fw_label(fw, width):
+        return _full_fw_label(fw).ljust(width) + f'FROM  {fw["from_version"]} - {fw["from_config"]}'
 
     def _delta_appropriate(fw):
         return fw['from_version'] == verinfo['JADE_VERSION'] and \
@@ -43,14 +43,15 @@ def get_fw_metadata(verinfo, release_data):
     i = 0
     print('Delta patches (faster)')
     deltas = list(filter(_delta_appropriate, release_data.get('delta', [])))
-    for i, label in enumerate((_delta_fw_label(fw) for fw in deltas), i + 1):  # 1 based index
-        print(f'{i})'.ljust(3), label)
+    just = max(len(name) for name in map(_full_fw_label, deltas)) + 2
+    for i, label in enumerate((_delta_fw_label(fw, just) for fw in deltas), i + 1):  # 1 based index
+        print(f'{i})'.ljust(4), label)
     print('-')
 
     print('Full firmware images')
     fullfws = release_data.get('full', [])
     for i, label in enumerate((_full_fw_label(fw) for fw in fullfws), i + 1):  # continue numbering
-        print(f'{i})'.ljust(3), label)
+        print(f'{i})'.ljust(4), label)
     print('-')
 
     numdeltas = len(deltas)

commit c11db205f0c362edd018c91b35640d5443ea405a

scripts: update jade_ota.py in line with update_jade_fw.py
Fixes bug introduced in 5f280bd363e2ade22564d74eb889d01e4344e0cd when
no deltas available, and applies that same changes to jade_ota.py.

jade_ota.py
@@ -60,8 +60,8 @@ def get_fw_metadata(release_data):
     def _full_fw_label(fw):
         return f'{fw["version"]} - {fw["config"]}'
 
-    def _delta_fw_label(fw):
-        return _full_fw_label(fw).ljust(18) + f'FROM  {fw["from_version"]} - {fw["from_config"]}'
+    def _delta_fw_label(fw, width):
+        return _full_fw_label(fw).ljust(width) + f'FROM  {fw["from_version"]} - {fw["from_config"]}'
 
     print('Full firmwares')
     fullfws = release_data.get('full', [])
@@ -71,8 +71,9 @@ def get_fw_metadata(release_data):
 
     print('Delta patches')
     deltas = release_data.get('delta', [])
-    for i, label in enumerate((_delta_fw_label(fw) for fw in deltas), i + 1):  # continue numbering
-        print(f'{i})'.ljust(3), label)
+    just = max(len(name) for name in map(_full_fw_label, deltas)) + 2 if deltas else 0
+    for i, label in enumerate((_delta_fw_label(fw, just) for fw in deltas), i + 1):  # continue
+        print(f'{i})'.ljust(4), label)
     print('-')
 
     selectedfw = int(input('Select firmware: '))

update_jade_fw.py
@@ -43,7 +43,7 @@ def get_fw_metadata(verinfo, release_data):
     i = 0
     print('Delta patches (faster)')
     deltas = list(filter(_delta_appropriate, release_data.get('delta', [])))
-    just = max(len(name) for name in map(_full_fw_label, deltas)) + 2
+    just = max(len(name) for name in map(_full_fw_label, deltas)) + 2 if deltas else 0
     for i, label in enumerate((_delta_fw_label(fw, just) for fw in deltas), i + 1):  # 1 based index
         print(f'{i})'.ljust(4), label)
     print('-')