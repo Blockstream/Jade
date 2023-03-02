e1428461de5a1a0e41043a9772438d66d839777a
Lawrence Nahum <lawrence@greenaddress.it>
2023-03-02 15:51:20 +0100

    CI: add check for Kconfig formatting, fix current formatting

--- format.sh
+++ format.sh
@@ -10,3 +10,8 @@ fi
 if [ -x "$(command -v pycodestyle)" ]; then
     pycodestyle --max-line-length=100 *.py pinserver/*.py pinserver/test/*.py jadepy/*.py tools/*.py
 fi
+
+KCONFIG_FILE=main/Kconfig.projbuild
+
+${IDF_PATH}/tools/ci/check_kconfigs.py ${KCONFIG_FILE} || true
+mv ${KCONFIG_FILE}.new ${KCONFIG_FILE}
