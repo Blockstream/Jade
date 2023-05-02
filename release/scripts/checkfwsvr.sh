Blockstream/Jade

commit 826c847e41bfd02fb5cafe76d0d89c1ddbb865d8

releases: pull hash files when downloading fwsvr directories

release/scripts/checkfwsvr.sh
@@ -56,6 +56,7 @@ do
           then
             echo "Missing: ${hwdir} - ${index} - ${fwfile}" >> ../"${MISSING}"
           fi
+          wget -O "${fwfile}" "${FWLOCATION}/${fwfile}.hash"
         fi
       done
     fi

commit 50d40a2b8fffe36400c76485c94cd5403341881e

releases: correct downloading hash files when fetching fwsvr directories
ie. fix issue introduced in 826c847e41bfd02fb5cafe76d0d89c1ddbb865d8

release/scripts/checkfwsvr.sh
@@ -56,7 +56,7 @@ do
           then
             echo "Missing: ${hwdir} - ${index} - ${fwfile}" >> ../"${MISSING}"
           fi
-          wget -O "${fwfile}" "${FWLOCATION}/${fwfile}.hash"
+          wget "${FWLOCATION}/${fwfile}.hash"
         fi
       done
     fi
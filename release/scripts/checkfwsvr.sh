50d40a2b8fffe36400c76485c94cd5403341881e
Jamie C. Driver <jamie@blockstream.io>
2023-05-02 12:21:41 +0100

    releases: correct downloading hash files when fetching fwsvr directories
    ie. fix issue introduced in 826c847e41bfd02fb5cafe76d0d89c1ddbb865d8

--- release/scripts/checkfwsvr.sh
+++ release/scripts/checkfwsvr.sh
@@ -56,7 +56,7 @@ do
           then
             echo "Missing: ${hwdir} - ${index} - ${fwfile}" >> ../"${MISSING}"
           fi
-          wget -O "${fwfile}" "${FWLOCATION}/${fwfile}.hash"
+          wget "${FWLOCATION}/${fwfile}.hash"
         fi
       done
     fi
