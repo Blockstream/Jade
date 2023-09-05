615655441d556975608ad8ff08fc99bdc22613dd
Lawrence Nahum <lawrence@greenaddress.it>
2023-09-05 15:16:46 +0200

    build: update config check tool to do prod before dev and add cleanup

--- tools/check_default_configs.sh
+++ tools/check_default_configs.sh
@@ -6,8 +6,9 @@ if [ -f /.dockerenv ]; then
 fi
 
 
-for filename in configs/*.defaults production/*.defaults; do
+for filename in production/*.defaults configs/*.defaults; do
     rm -fr sdkconfig sdkconfig.defaults
     idf.py -D SDKCONFIG_DEFAULTS="${filename}" reconfigure save-defconfig
     tail -n +4 sdkconfig.defaults > ${filename}
 done
+rm -fr sdkconfig sdkconfig.defaults build
