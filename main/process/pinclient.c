Blockstream/Jade

commit 12442a412e578a9796bdd9740a058284a667c7ca

pinclient: allow the pinserver protocol to be cancelled/abandoned

main/process/pinclient.c
@@ -39,7 +39,7 @@ static const char PINSERVER_DOC_SET_PIN[] = "set_pin";
 
 // Success or failure, and any error data to send in reply message
 typedef struct {
-    enum { SUCCESS = 0, CAN_RETRY, FAILURE } result;
+    enum { SUCCESS = 0, CAN_RETRY, FAILURE, CANCELLED } result;
 
     uint32_t errorcode;
     const char* message;
@@ -375,7 +375,10 @@ static pinserver_result_t start_handshake(jade_process_t* process, pin_keys_t* p
     // Await a 'handshake_init' message
     jade_process_load_in_message(process, true);
 
-    if (!IS_CURRENT_MESSAGE(process, "handshake_init")) {
+    if (IS_CURRENT_MESSAGE(process, "cancel")) {
+        // Cancelled
+        RETURN_RESULT(CANCELLED, 0, NULL);
+    } else if (!IS_CURRENT_MESSAGE(process, "handshake_init")) {
         // Protocol error
         RETURN_RESULT(FAILURE, CBOR_RPC_PROTOCOL_ERROR, "Unexpected message, expecting 'handshake_init'");
     }
@@ -450,7 +453,10 @@ static pinserver_result_t complete_handshake(
     // Await a 'complete_handshake' message
     jade_process_load_in_message(process, true);
 
-    if (!IS_CURRENT_MESSAGE(process, "handshake_complete")) {
+    if (IS_CURRENT_MESSAGE(process, "cancel")) {
+        // Cancelled
+        RETURN_RESULT(CANCELLED, 0, NULL);
+    } else if (!IS_CURRENT_MESSAGE(process, "handshake_complete")) {
         // Protocol error
         RETURN_RESULT(FAILURE, CBOR_RPC_PROTOCOL_ERROR, "Unexpected message, expecting 'handshake_complete'");
     }
@@ -666,16 +672,16 @@ static bool get_pinserver_aeskey(jade_process_t* process, const uint8_t* pin, co
             continue;
         }
 #endif
-        if (pir.result != SUCCESS) {
-            // If failed, send reject message
+        // If failed, send reject message
+        if (pir.result == FAILURE) {
             JADE_LOGE("Failed to complete pinserver interaction");
             jade_process_reject_message(process, pir.errorcode, pir.message, NULL);
             await_error_activity("Network or server error");
             return false;
         }
 
-        // Otherwise all good, return true
-        return true;
+        // Otherwise if all good, return true
+        return pir.result == SUCCESS;
     }
 }
 

main/process/dashboard.c
@@ -351,6 +351,9 @@ static void dispatch_message(jade_process_t* process)
     } else if (IS_METHOD("auth_user")) {
         JADE_LOGD("Received auth-user request");
         task_function = auth_user_process;
+    } else if (IS_METHOD("cancel")) {
+        // 'cancel' is completely ignored (as nothing is 'in-progress' to cancel)
+        JADE_LOGD("Received 'cancel' request - no-op");
     } else if (IS_METHOD("ota")) {
         // OTA is allowed if either:
         // a) User has passed PIN screen and has unlocked Jade saved wallet

commit 62e9b55483c906cf27e0823af5602d634c236770

pinclient: send error reply when user opts not to retry network message
Issue caused by changes in 12442a412e578a9796bdd9740a058284a667c7ca

main/process/pinclient.c
@@ -673,8 +673,9 @@ static bool get_pinserver_aeskey(jade_process_t* process, const uint8_t* pin, co
             continue;
         }
 #endif
-        // If failed, send reject message
-        if (pir.result == FAILURE) {
+        // If failed or abandoned, send reject message
+        // NOTE: 'CANCELLED' is deliberately 'silent'
+        if (pir.result != SUCCESS && pir.result != CANCELLED) {
             JADE_LOGE("Failed to complete pinserver interaction");
             jade_process_reject_message(process, pir.errorcode, pir.message, NULL);
             await_error_activity("Network or server error");