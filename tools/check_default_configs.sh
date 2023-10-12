9886c4e1edfb036d8ec6ea33036a3c0c03d0fb14
Lawrence Nahum <lawrence@greenaddress.it>
2023-10-12 17:10:11 +0200

    build: add support for esp32 s3 builds in check_default_configs.sh

--- tools/check_default_configs.sh
+++ tools/check_default_configs.sh
@@ -5,10 +5,16 @@ if [ -f /.dockerenv ]; then
     . ${HOME}/esp/esp-idf/export.sh
 fi
 
-
 for filename in production/*.defaults configs/*.defaults; do
-    rm -fr sdkconfig sdkconfig.defaults
-    idf.py -D SDKCONFIG_DEFAULTS="${filename}" reconfigure save-defconfig
+    rm -fr sdkconfig sdkconfig.defaults build
+
+    if [[ $filename == *"s3"* ]]; then
+        esp_variant=esp32s3
+    else
+        esp_variant=esp32
+    fi
+
+    idf.py -D SDKCONFIG_DEFAULTS="${filename}" set-target ${esp_variant} reconfigure save-defconfig
     tail -n +4 sdkconfig.defaults > ${filename}
 done
 rm -fr sdkconfig sdkconfig.defaults build
