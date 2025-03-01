debd1c0cc61e2caff924a6cf4a1f5aa2745aeb24
Jon Griffiths <jon_p_griffiths@yahoo.com>
2025-03-01 13:35:55 +1300

    sign_psbt: add support for querying info from the set of keys a key iterator points to

--- main/utils/psbt.c
+++ main/utils/psbt.c
@@ -56,9 +56,47 @@ bool key_iter_next(key_iter* iter)
     return iter->is_valid;
 }
 
-bool key_iter_get_path(const key_iter* iter, uint32_t* path, const size_t path_len, size_t* written)
+size_t key_iter_get_num_keys(const key_iter* iter)
 {
     const struct wally_map* keypaths = key_iter_get_keypaths(iter);
-    int ret = wally_map_keypath_get_item_path(keypaths, iter->key_index, path, path_len, written);
+    return keypaths->num_items;
+}
+
+bool key_iter_contains_pubkey(const key_iter* iter, const uint8_t* pubkey, const size_t pubkey_len)
+{
+    const struct wally_map* keypaths = key_iter_get_keypaths(iter);
+    size_t written;
+    const int ret = wally_map_find(keypaths, pubkey, pubkey_len, &written);
+    return ret == WALLY_OK && written != 0;
+}
+
+bool key_iter_get_pubkey_at(const key_iter* iter, const size_t key_index, uint8_t* pubkey, const size_t pubkey_len)
+{
+    const struct wally_map* keypaths = key_iter_get_keypaths(iter);
+    size_t written;
+    const int ret = wally_map_get_item_key(keypaths, key_index, pubkey, pubkey_len, &written);
+    // written must exactly match pubkey_len, so that we only match
+    // compressed/uncompressed pubkeys depending on the callers intent
+    return ret == WALLY_OK && written == pubkey_len;
+}
+
+void key_iter_get_fingerprint_at(
+    const key_iter* iter, const size_t key_index, uint8_t* fingerprint, const size_t fingerprint_len)
+{
+    const struct wally_map* keypaths = key_iter_get_keypaths(iter);
+    const int ret = wally_map_keypath_get_item_fingerprint(keypaths, key_index, fingerprint, fingerprint_len);
+    JADE_WALLY_VERIFY(ret);
+}
+
+bool key_iter_get_path_at(
+    const key_iter* iter, const size_t key_index, uint32_t* path, const size_t path_len, size_t* written)
+{
+    const struct wally_map* keypaths = key_iter_get_keypaths(iter);
+    const int ret = wally_map_keypath_get_item_path(keypaths, key_index, path, path_len, written);
     return ret == WALLY_OK && *written <= path_len;
+}
+
+bool key_iter_get_path(const key_iter* iter, uint32_t* path, const size_t path_len, size_t* written)
+{
+    return key_iter_get_path_at(iter, iter->key_index, path, path_len, written);
 }
--- main/utils/psbt.h
+++ main/utils/psbt.h
@@ -40,4 +40,19 @@ bool key_iter_next(key_iter* iter);
 // Get the path to the key the iterator current points to
 bool key_iter_get_path(const key_iter* iter, uint32_t* path, size_t path_len, size_t* written);
 
+// Get the number of keys in the keypaths the iterator current points to
+size_t key_iter_get_num_keys(const key_iter* iter);
+
+// Returns true if the keypaths the iterator current points to contain `pubkey`
+bool key_iter_contains_pubkey(const key_iter* iter, const uint8_t* pubkey, size_t pubkey_len);
+
+// Get the public key of the `key_index`th key in the keypath the iterator current points to
+bool key_iter_get_pubkey_at(const key_iter* iter, size_t key_index, uint8_t* pubkey, size_t pubkey_len);
+
+// Get the fingerprint of the `key_index`th key in the keypath the iterator current points to
+void key_iter_get_fingerprint_at(const key_iter* iter, size_t key_index, uint8_t* fingerprint, size_t fingerprint_len);
+
+// Get the path to the `key_index`th key in the keypath the iterator current points to
+bool key_iter_get_path_at(const key_iter* iter, size_t key_index, uint32_t* path, size_t path_len, size_t* written);
+
 #endif /* PSBT_H_ */
