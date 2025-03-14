7a8de01673ef5d0772749717d72a1844ddc400fa
Caio Cesar Ferreira <caiocfer@gmail.com>
2025-03-13 21:35:49 -0300

    Fix camera OV3660 error
    This fixes the issues that occurred when attempting to read a QR code
    using the OV3660 camera.
    
    Signed-off-by: Caio Cesar Ferreira <caiocfer@gmail.com>

--- main/camera.c
+++ main/camera.c
@@ -306,14 +306,17 @@ static void jade_camera_init(void)
             JADE_LOGE("Failed to set camera hmirror/vflip, returned: %d/%d", hret, vret);
         }
     }
+
+    // OV3660 needs a vertical flip for the ESP32 Wrover Cam
     // OV5640 needs vertical flip for T-Display S3 PRO
-    else if (camera_info->model == CAMERA_OV5640) {
+    else if (camera_info->model == CAMERA_OV3660 || camera_info->model == CAMERA_OV5640) {
         JADE_ASSERT(camera_sensor->set_vflip);
         const int vret = camera_sensor->set_vflip(camera_sensor, 1);
         if (vret) {
             JADE_LOGE("Failed to set camera vflip, returned: %d", vret);
         }
     }
+
 #if defined(CONFIG_DISPLAY_TOUCHSCREEN)
     touchscreen_deinit();
     touchscreen_init();
