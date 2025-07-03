35ad314fb991af92bb6975c065c5ba5b7e44cfe3
Jon Griffiths <jon_p_griffiths@yahoo.com>
2025-07-03 20:28:16 +1200

    sign_psbt: use shared signing tx validation code
    This means we don't have to find the fee ourselves, and ensures that
    the checks we perform (e.g. duplicate fees) are consistent whether we
    are signing a tx or psbt.

--- main/process/sign_psbt.c
+++ main/process/sign_psbt.c
@@ -450,23 +450,17 @@ static bool get_suitable_descriptor_record(const key_iter* iter, const uint32_t*
 // Examine outputs for change we can automatically validate
 static bool validate_outputs(const network_t network_id, struct wally_psbt* psbt, const uint8_t signing_flags,
     const char* wallet_name, const multisig_data_t* multisig_data, const descriptor_data_t* descriptor,
-    output_info_t* output_info, uint64_t* explicit_fee, const char** errmsg)
+    output_info_t* output_info, const char** errmsg)
 {
     JADE_ASSERT(network_id != NETWORK_NONE);
     JADE_ASSERT(psbt);
     // wallet_name, multisig_data and descriptor optional
     JADE_ASSERT(output_info);
-    JADE_INIT_OUT_SIZE(explicit_fee);
     JADE_INIT_OUT_PPTR(errmsg);
 
     const bool is_liquid = network_is_liquid(network_id);
     JADE_ASSERT(!multisig_data || !descriptor); // cannot have both
     JADE_ASSERT(!is_liquid || !descriptor); // atm do not support liquid descriptors
-
-    uint8_t policy_asset[ASSET_TAG_LEN];
-    if (is_liquid) {
-        network_to_policy_asset(network_id, policy_asset, sizeof(policy_asset));
-    }
 
     key_iter iter; // Holds any public key in use
 
@@ -500,20 +494,10 @@ static bool validate_outputs(const network_t network_id, struct wally_psbt* psbt
             }
 
             if (wally_psbt_get_output_script_len(psbt, index, &written) != WALLY_OK || !written) {
-                if (outinfo->flags & OUTPUT_FLAG_CONFIDENTIAL || !(outinfo->flags & OUTPUT_FLAG_HAS_UNBLINDED)) {
-                    *errmsg = "Fee output (without script) cannot be blinded";
-                    return false;
-                }
-
-                if (memcmp(outinfo->asset_id, policy_asset, sizeof(policy_asset))) {
-                    *errmsg = "Unexpected fee output (without script) asset-id";
-                    return false;
-                }
-
-                // Tally fees
-                *explicit_fee += outinfo->value;
-
-                // If is fee output, can't be change, so may as well skip now
+                // Fee output
+                JADE_ASSERT(!(outinfo->flags & OUTPUT_FLAG_CONFIDENTIAL));
+                JADE_ASSERT(outinfo->flags & OUTPUT_FLAG_HAS_UNBLINDED);
+                // Fee outputs can't be change, so may as well skip now
                 JADE_ASSERT(!(outinfo->flags & (OUTPUT_FLAG_VALIDATED | OUTPUT_FLAG_CHANGE)));
                 continue;
             }
@@ -674,6 +658,7 @@ int sign_psbt(const network_t network_id, struct wally_psbt* psbt, const char** 
     JADE_ASSERT(psbt);
     JADE_INIT_OUT_PPTR(errmsg);
     JADE_ASSERT(network_id != NETWORK_NONE);
+    int retval = 0;
 
     size_t is_elements = 0;
     JADE_WALLY_VERIFY(wally_psbt_is_elements(psbt, &is_elements));
@@ -682,7 +667,9 @@ int sign_psbt(const network_t network_id, struct wally_psbt* psbt, const char** 
         return CBOR_RPC_BAD_PARAMETERS;
     }
 
+    uint64_t explicit_fee = 0;
     struct wally_tx* tx = NULL; // Holds the extracted tx
+
     // Fetch the tx to sign
     if (psbt->version == WALLY_PSBT_VERSION_0) {
         tx = psbt->tx; // For v0, use the PSBT tx directly
@@ -692,9 +679,13 @@ int sign_psbt(const network_t network_id, struct wally_psbt* psbt, const char** 
     }
     JADE_ASSERT(tx && tx->num_inputs == psbt->num_inputs && tx->num_outputs == psbt->num_outputs);
 
+    if (!params_txn_validate(network_id, for_liquid, tx, &explicit_fee, errmsg)) {
+        retval = CBOR_RPC_BAD_PARAMETERS;
+        goto cleanup;
+    }
+
     key_iter iter; // Holds any public/private key in use
     SENSITIVE_PUSH(&iter, sizeof(iter));
-    int retval = 0;
 
     // We track if the type of the inputs we are signing changes (ie. single-sig vs
     // green/multisig/other) so we can show a warning to the user if so.
@@ -842,9 +833,8 @@ int sign_psbt(const network_t network_id, struct wally_psbt* psbt, const char** 
     } // iterate keys
 
     // Examine outputs for liquid unblinded info and fees, and for change we can automatically validate
-    uint64_t explicit_fee = 0;
-    if (!validate_outputs(network_id, psbt, signing_flags, wallet_name, multisig_data, descriptor, output_info,
-            &explicit_fee, errmsg)) {
+    if (!validate_outputs(
+            network_id, psbt, signing_flags, wallet_name, multisig_data, descriptor, output_info, errmsg)) {
         // errmsg will be populated
         retval = CBOR_RPC_BAD_PARAMETERS;
         goto cleanup;
