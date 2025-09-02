de4e7cd52bbae28383b2f21860adc0dcb9a00bfb
Jon Griffiths <jon_p_griffiths@yahoo.com>
2025-09-03 09:43:28 +1200

    jadepy: allow additional_info for sign_psbt

--- jadepy/jade.py
+++ jadepy/jade.py
@@ -1941,7 +1941,7 @@ class JadeAPI:
         # Send inputs and receive signatures
         return self._send_tx_inputs(base_id, inputs, use_ae_signatures, use_legacy)
 
-    def sign_psbt(self, network, psbt):
+    def sign_psbt(self, network, psbt, additional_info=None):
         """
         RPC call to sign a passed psbt as required
 
@@ -1953,6 +1953,9 @@ class JadeAPI:
         psbt : bytes
             The psbt formatted as bytes
 
+        additional_info: dict, optional
+            Extra data about the transaction.  See sign_liquid_tx for details.
+
         Returns
         -------
         bytes
@@ -1960,6 +1963,8 @@ class JadeAPI:
         """
         # Send PSBT message
         params = {'network': network, 'psbt': psbt}
+        if additional_info:
+            params['additional_info'] = additional_info
         msgid = str(random.randint(100000, 999999))
         request = self.jade.build_request(msgid, 'sign_psbt', params)
         self.jade.write_request(request)
