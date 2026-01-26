cc7907b2fa18fbf5c8a65b2385a76fd3d003a24b
Jon Griffiths <jon_p_griffiths@yahoo.com>
2026-01-26 22:45:13 +1300

    ci: increase the time allowance for selfcheck

--- test_jade.py
+++ test_jade.py
@@ -3885,7 +3885,7 @@ def run_interface_tests(jadeapi,
         if not isble:
             time_ms = jadeapi.run_remote_selfcheck()
             logger.info(f'selfcheck time: {time_ms} ms')
-            assert qemu or time_ms < (52000 if is_s3 else 132000)
+            assert qemu or time_ms < (54000 if is_s3 else 134000)
 
         # Test good pinserver handshake, and also 'bad sig' pinserver
         test_handshake(jadeapi.jade)
