4cec7ba57be8d4442c33f7830ed12e4694684557
Jon Griffiths <jon_p_griffiths@yahoo.com>
2026-04-08 13:14:51 +1200

    assets: rename L-BTC issuer from "peg-in" to "federation"

--- components/assets/assets_snapshot.c
+++ components/assets/assets_snapshot.c
@@ -10,8 +10,8 @@
 static const snapshot_asset_info_t assets_snapshot[] = {
     // These are not present in the asset registry data, or override what is there (as they are first).
     // These are the 'policy_assets' of liquid and localtest-liquid
-    ASSET_INFO("6f0279e9ed041c3d710a9f57d0c02928416460c4b722ae3457a11eec381c526d", "L-BTC", "peg-in", 8),  // liquid
-    ASSET_INFO("5ac9f65c0efcc4775e0baec4ec03abdde22473cd3cf33c0419ca290e0751b225", "L-BTC", "peg-in (localtest)", 8),  // localtest-liquid
+    ASSET_INFO("6f0279e9ed041c3d710a9f57d0c02928416460c4b722ae3457a11eec381c526d", "L-BTC", "federation", 8),  // liquid
+    ASSET_INFO("5ac9f65c0efcc4775e0baec4ec03abdde22473cd3cf33c0419ca290e0751b225", "L-BTC", "federation (localtest)", 8),  // localtest-liquid
 
     // Include the file generated from the asset registry json data file
     #include "asset_data.inc"
@@ -21,7 +21,7 @@ static const snapshot_asset_info_t assets_snapshot[] = {
 static const snapshot_asset_info_t assets_snapshot_testnet[] = {
     // These are not present in the asset registry data, or override what is there (as they are first).
     // This is the 'policy_asset' of testnet-liquid
-    ASSET_INFO("144c654344aa716d6f3abcc1ca90e5641e4e2a7f633bc09fe3baf64585819a49", "L-TEST", "peg-in (testnet)", 8),  // testnet-liquid
+    ASSET_INFO("144c654344aa716d6f3abcc1ca90e5641e4e2a7f633bc09fe3baf64585819a49", "L-TEST", "federation (testnet)", 8),  // testnet-liquid
 
     // Include the file generated from the testnet asset registry json data file
     #include "asset_data_testnet.inc"
