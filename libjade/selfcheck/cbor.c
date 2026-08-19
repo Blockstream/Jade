c1f0d4ff710c78c8529ef5f8912a64aa87e5ed5d
Mike Tolkachev <mtolkachev@blockstream.com>
2026-08-19 19:46:14 -0300

    cbor: add test for parser recursion limit

--- libjade/selfcheck/cbor.c
+++ libjade/selfcheck/cbor.c
@@ -43,9 +43,44 @@ static bool test_invalid_cbor(void)
     return true;
 }
 
+static bool test_parser_recursion_limit(void)
+{
+    JADE_STATIC_ASSERT(CBOR_PARSER_MAX_RECURSIONS > 0);
+
+    enum { MIN_DEPTH = CBOR_PARSER_MAX_RECURSIONS - 1, MAX_DEPTH = CBOR_PARSER_MAX_RECURSIONS + 1 };
+    uint8_t cbor[MAX_DEPTH + 1];
+
+    for (size_t depth = MIN_DEPTH; depth <= MAX_DEPTH; ++depth) {
+        // A single-item array at each level, terminated by a null leaf:
+        // [[[...[null]...]]]
+        memset(cbor, CborArrayType | 1, depth); // one element arrays
+        cbor[depth] = CborNullType;
+
+        CborParser parser;
+        CborValue root;
+        const CborError init_res = cbor_parser_init(cbor, depth + 1, CborValidateBasic, &parser, &root);
+        JADE_ASSERT(init_res == CborNoError);
+        JADE_ASSERT(cbor_value_is_array(&root));
+
+        const CborError expected_err = depth <= CBOR_PARSER_MAX_RECURSIONS ? CborNoError : CborErrorNestingTooDeep;
+        const CborError err = cbor_value_validate_basic(&root);
+        const char* err_text = err == CborNoError ? "ok" : cbor_error_string(err);
+        JADE_LOGD("Validating CBOR with nesting depth %zu: %s", depth, err_text);
+        if (err != expected_err) {
+            JADE_LOGE("Failed CBOR validation case with nesting depth %zu", depth);
+            return false;
+        }
+    }
+
+    return true;
+}
+
 bool debug_selfcheck(jade_process_t* process)
 {
     if (!test_invalid_cbor()) {
+        FAIL();
+    }
+    if (!test_parser_recursion_limit()) {
         FAIL();
     }
     return true;
